445bdfb226e35d811c529ffe934b9de490edc00b
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-14 11:05:23 +0200

    Ejercicio1.java

--- Tema02/Ejercicio1.java
+++ Tema02/Ejercicio1.java
@@ -3,8 +3,30 @@
  *
  * @author Alberto Parra Toval
  */
-public class ejercicio1 {
+public class Ejercicio1 {
   public static void main(String[] args) {
+    //Variables con la que haremos las operaciones
+    int y;  y = 999;
+    int x;  x = 144;
     
+    //Suma    
+    int Suma;
+    Suma = x + y;    
+    System.out.println(x + y); 
+    
+    //Resta
+    int Resta;
+    Resta = x - y;
+	System.out.println(x - y);
+	
+	//Multiplicación
+	int Multiplicacion;
+	Multiplicacion = x * y;
+	System.out.println(x * y);
+	
+	//Division
+    double Division;
+    Division = (double) y / (double) x;
+    System.out.println(Division);  
   }
 }
