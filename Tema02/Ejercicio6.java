8c742e49c96ad41f7607bae97f7b00881ac3d0ed
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-14 18:41:43 +0200

    Ejercicio6.java

---
+++ Tema02/Ejercicio6.java
@@ -0,0 +1,15 @@
+/**
+ * Ejercicio 6
+ *
+ * @author Alberto Parra Toval
+ */
+public class Ejercicio6 {
+  public static void main(String[] args) {
+    
+    double euros = 340.00;
+    
+    int Iva = (int) (euros / 0.79);
+    
+    System.out.print(" La factura será de " + Iva + " euros "); 
+  }
+}
