1210cee86ac178644cbbb821c7ddea6f95710493
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-14 11:14:52 +0200

    Ejercicio2.java

---
+++ Tema02/Ejercicio2.java
@@ -0,0 +1,13 @@
+/**
+ * Ejercicio 1
+ *
+ * @author Alberto Parra Toval
+ */
+public class Ejercicio2 {
+  public static void main(String[] args) {
+    String miNombre;
+    
+    
+    System.out.println("Mi nombre es:" + miNombre);
+  }
+}
