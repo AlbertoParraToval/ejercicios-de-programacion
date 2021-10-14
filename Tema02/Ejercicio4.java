d864ee0b9d0d3a53f69b339a38bfe0f7b60d685c
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-14 18:34:24 +0200

    Ejercicio4.java

---
+++ Tema02/Ejercicio4.java
@@ -0,0 +1,15 @@
+/**
+ * Ejercicio 4
+ *
+ * @author Alberto Parra Toval
+ */
+public class Ejercicio4 {
+  public static void main(String[] args) {
+    
+    double euros = 20.00;
+    
+    int pesetas = (int) (euros * 166.386);
+    
+    System.out.print(euros + " euros son " + pesetas + " pesetas. "); 
+  }
+}
