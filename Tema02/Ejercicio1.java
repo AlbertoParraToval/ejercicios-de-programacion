f89a72c5114852f5890060a0b388ce1bc0878a35
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-15 08:38:04 +0200

    Ejercicio1.java

--- Tema02/Ejercicio1.java
+++ Tema02/Ejercicio1.java
@@ -12,21 +12,21 @@ public class Ejercicio1 {
     //Suma    
     int Suma;
     Suma = x + y;    
-    System.out.println(x + y); 
+    System.out.println(" El resultado de la suma es " + Suma); 
     
     //Resta
     int Resta;
     Resta = x - y;
-	System.out.println(x - y);
+	System.out.println(" El resultado de la resta es " + Resta);
 	
 	//Multiplicación
 	int Multiplicacion;
 	Multiplicacion = x * y;
-	System.out.println(x * y);
+	System.out.println(" El resultado de la multiplicación es " + Multiplicacion);
 	
 	//Division
     double Division;
     Division = (double) y / (double) x;
-    System.out.println(Division);  
-  }
+    System.out.println(" El resultado de la división es " + Division);  
+  } 
 }
