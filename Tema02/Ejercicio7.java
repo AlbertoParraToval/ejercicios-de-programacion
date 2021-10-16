98306df5b0e4e0b8cdcb37b569f49f0009630c10
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-16 14:39:10 +0200

    Ejercicio7.java

---
+++ Tema02/Ejercicio7.java
@@ -0,0 +1,17 @@
+/**
+ * Ejercicio 7
+ *
+ * @author Alberto Parra Toval
+ */
+public class Ejercicio7 {
+  public static void main(String[] args) {
+
+	char texto2 = 'N';
+	char texto3 = 'o';
+	
+
+    String texto1 = "me gustan las peleas de boxeo";
+
+    System.out.println(texto2 + "" + texto3 + " " + texto1);
+ }
+}
