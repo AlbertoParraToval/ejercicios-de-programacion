2783bbbb996aa388b828e2e4d128e18a82520328
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-16 14:48:20 +0200

    Ejercicio8.java

---
+++ Tema02/Ejercicio8.java
@@ -0,0 +1,18 @@
+/**
+ * Ejercicio 8
+ *
+ * @author Alberto Parra Toval
+ */
+public class Ejercicio8 {
+  public static void main(String[] args) {
+    char letra1 = 'V';
+	char letra2 = 'e';
+	char letra3 = 'r';
+	char letra4 = 's';
+	char letra5 = 'o';
+	
+	String palabra = "" + letra1 + letra2 + letra3 + letra4 + letra5;
+	
+	System.out.println(palabra); 
+  }
+}
