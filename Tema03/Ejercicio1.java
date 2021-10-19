8b17bc6e9902b298603e81d58287015a2f7f5128
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-19 08:27:59 +0200

    Ejercicio1.java

---
+++ Tema03/Ejercicio1.java
@@ -0,0 +1,27 @@
+/**
+* Realiza un programa que pida dos números y que luego muestre el resultado
+  de su multiplicación.
+*
+* @author Alberto Parra 
+*/
+public class Ejercicio1 {
+public static void main(String[] args) {
+	String linea;
+	System.out.print("Por favor, introduce un número: ");
+	linea = System.console().readLine();
+	int primerNumero;
+	primerNumero = Integer.parseInt( linea );
+
+	System.out.print("introduce otro, por favor: ");
+	linea = System.console().readLine();
+	int segundoNumero;
+	segundoNumero = Integer.parseInt( linea );
+
+	int total;
+	total = (segundoNumero * primerNumero);
+	
+	System.out.print("La multiplicación de estos dos números son " + total);
+	
+}
+}
+
---
+++ Tema03/Ejercicio6.java
@@ -0,0 +1,12 @@
+/**
+* 
+*
+* @author Alberto Parra 
+*/
+public class Ejercicio6 {
+public static void main(String[] args) {
+
+
+
+ }
+}
