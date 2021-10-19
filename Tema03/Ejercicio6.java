8e72e36a8b68837ffa31833a3c56cafd8fe74129
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-19 10:56:57 +0200

    Ejercicio6.java

--- Tema03/Ejercicio6.java
+++ Tema03/Ejercicio6.java
@@ -7,14 +7,16 @@ public class Ejercicio6 {
 public static void main(String[] args) {
 	System.out.println("Este programa clacula la superficie de un rectángulo.");
 	
+	//Definimos la base
 	System.out.println("Introduce la base del rectángulo en cm:");
 	int base = Integer.parseInt(System.console().readLine() );
 	
+	//Definimos la altura
 	System.out.print("Introduce la altura del rectángulo en cm:");
 	int altura = Integer.parseInt(System.console().readLine() );
 	
-	double resultado = ((base * altura)/2);
-	 
+	//Realizamos la Operación
+	double resultado = ((base * altura)/2); 
 	System.out.print("El área del rectángulo es " + resultado + " cm");
  }
 }
