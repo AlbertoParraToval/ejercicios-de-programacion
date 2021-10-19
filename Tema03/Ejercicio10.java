b061bd7e16fa86d3fd2b7eb8c1e7a1328a0e455e
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-19 11:11:33 +0200

    Ejercicio10.java

--- Tema03/Ejercicio10.java
+++ Tema03/Ejercicio10.java
@@ -6,11 +6,12 @@
 public class Ejercicio10 {
 public static void main(String[] args) {
 	System.out.println("Este programa convierte de kb a Mb.");
-	
+	//Introducir el valor a convertir
 	System.out.print("Introduce el valor en kb");
     int Mb = Integer.parseInt(System.console().readLine() );;
     double Conversion = 1000;
     
+    //Realizamos la conversión
     double Kb = (Mb * Conversion);
     System.out.printf("El resultado de la conversión es " + Kb + "Kb");
  }
