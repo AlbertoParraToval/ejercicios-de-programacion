840cd16339f5ff77d8ed9d2ae5f38f366e739427
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-19 09:29:25 +0200

    Ejercicio7.java

---
+++ Tema03/Ejercicio7.java
@@ -0,0 +1,28 @@
+/**
+* Escribe un programa que calcule el total de una factura a partir de la base
+  imponible.
+*
+* @author Alberto Parra 
+*/
+public class Ejercicio7 {
+public static void main(String[] args) {
+	System.out.println("Este programa calcula la factura total");
+	
+	System.out.println("Introduce el primer pedido");
+	int pedido1 = Integer.parseInt(System.console().readLine() );
+	
+	System.out.println("Introduce el segundo pedido");
+	int pedido2 = Integer.parseInt(System.console().readLine() );
+	
+	System.out.println("Introduce el tercero pedido");
+	int pedido3 = Integer.parseInt(System.console().readLine() );
+	
+	System.out.println("Introduce el cuarto pedido");
+	int pedido4 = Integer.parseInt(System.console().readLine() );
+	
+	double pedidoTotal = (pedido1 + pedido2 + pedido3 + pedido4);
+	System.out.println("La factura total será de " + pedidoTotal + "euros.");
+	
+	
+ }
+}
---
+++ Tema03/Ejercicio8.java
@@ -0,0 +1,13 @@
+/**
+* Escribe un programa que calcule el salario semanal de un empleado en base a
+  las horas trabajadas, a razón de 12 euros la hora.
+*
+* @author Alberto Parra 
+*/
+public class Ejercicio8 {
+public static void main(String[] args) {
+	System.out.println("Este programa clacula la superficie de un rectángulo.");
+	
+	
+ }
+}
