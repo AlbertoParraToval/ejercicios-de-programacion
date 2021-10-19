a75ebf25f0471a08a367c6da5fbce26be95bec50
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-19 08:58:32 +0200

    Ejercicio3.java

--- Tema03/Ejercicio3.java
+++ Tema03/Ejercicio3.java
@@ -7,8 +7,12 @@
 */
 public class Ejercicio3 {
 public static void main(String[] args) {
-
-
-
+	
+	System.out.print("Introduce el valor de pesetas que quieres conventir: ");
+    int pesetas = Integer.parseInt(System.console().readLine() );;
+    double Conversion = 166.38;
+    
+    double euros = (pesetas / Conversion);
+    System.out.printf(" %d pesetas en euros son: %.2f €" ,pesetas ,euros);
  }
 }
