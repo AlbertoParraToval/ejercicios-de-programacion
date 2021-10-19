143f0921108b8e3248ca31a5325ed6546125b019
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-19 11:02:47 +0200

    Ejercicio8.java

--- Tema03/Ejercicio8.java
+++ Tema03/Ejercicio8.java
@@ -7,13 +7,13 @@
 public class Ejercicio8 {
 public static void main(String[] args) {
 	System.out.println("Este programa calcula el salario en base al número de horas.");
-	
+	//Preguntamos el numero de horas al usuario
 	System.out.println("Número de horas trabajando a la semana.");
 	int horas = Integer.parseInt(System.console().readLine() );
-	
+	//Definimos el salario fijo
 	int salarioHora;
 	salarioHora = 12;
-	
+	//Realizamos la operación
 	double operacion = (horas * salarioHora);
 	System.out.println("El salario de nuestro trabajador es " + operacion + " euros");
  }
