36579662619ff0eb66b32f6237e23308678dd94f
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-19 11:07:02 +0200

    Ejercicio9.java

--- Tema03/Ejercicio9.java
+++ Tema03/Ejercicio9.java
@@ -15,13 +15,13 @@ public static void main(String[] args) {
 	System.out.println("Introduce el radio del cono en cm:");
 	int radio = Integer.parseInt(System.console().readLine() );
 	//Valor de la altura
-	System.out.println("Introduce la base del cono en cm:");
+	System.out.println("Introduce la altura del cono en cm:");
 	int altura = Integer.parseInt(System.console().readLine() );
 	//Realizamos las operaciones
 	double resultado1 = (radio * radio);
 	double resultado2 = (resultado1 * numeroPi);
 	double resultadoFinal = (resultado2 / 3);
-	
+	//Lo muestra en pantalla
 	System.out.print("El volumen de nuestro cono será de " + resultadoFinal + "cm³");
 	
 	
