9889c75685c28dfe5fa4eecbff2dc39a8857c126
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-19 11:00:55 +0200

    Ejercicio7.java

--- Tema03/Ejercicio7.java
+++ Tema03/Ejercicio7.java
@@ -6,9 +6,11 @@
 */
 public class Ejercicio7 {
 public static void main(String[] args) {
+	//Preguntamos que el usuario imponga la baseImponible
 	System.out.print("Por favor, introduzca la base imponible (precio del artículo sin IVA): ");
     double baseImponible = Double.parseDouble(System.console().readLine());
-
+	
+	//Ejecutamos dicha base imponible con el Iva incluido
     System.out.printf("Base imponible %8.2f\n", baseImponible);
     System.out.printf("IVA            %8.2f\n", (baseImponible * 0.21));
     System.out.printf("-----------------------\n");
