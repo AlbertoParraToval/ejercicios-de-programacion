0eb91f43d36e0da1bb58f8e99f169c40aabbaa64
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-21 16:44:51 +0200

    Ejercicios

---
+++ Tema04/Ejercicio10Tema4.java
@@ -0,0 +1,18 @@
+/**
+* 	Escribe un programa que nos diga el horóscopo a partir del día y el mes de
+	nacimiento
+*
+* @author Alberto Parra 
+*/
+public class Ejercicio10Tema4 {
+	public static void main(String[] args) {
+		
+		System.out.print("Introduzca el día: ");
+		int dia = Integer.parseInt(System.console().readLine());
+		System.out.print("Introduzca el mes: ");
+		int mes = Integer.parseInt(System.console().readLine());
+		System.out.print("Introduzca la hora: ");
+		double hora = Double.parseDouble(System.console().readLine());
+  }
+}
+
--- Tema04/Ejercicio6Tema4.java
+++ Tema04/Ejercicio6Tema4.java
@@ -2,7 +2,7 @@
 * 	Realiza un programa que calcule el tiempo que tardará en caer un objeto desde
 una altura h. Aplica la fórmula t =
 √2h
-g
+g                    
 siendo g = 9.81m/s
 2
 *
