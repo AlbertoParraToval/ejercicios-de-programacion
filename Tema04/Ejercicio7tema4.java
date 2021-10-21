d75480eff4e01ad0b828b187b0534fa985b5a9b0
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-21 16:09:55 +0200

    Ejercicio7

---
+++ Tema04/Ejercicio7tema4.java
@@ -0,0 +1,18 @@
+/**
+* Realiza un programa que calcule la media de tres notas
+*
+* @author Alberto Parra 
+*/
+public class Ejercicio7tema4 {
+	public static void main(String[] args) {
+		System.out.print("Nota 1: ");
+		Double nota1 = Double.parseDouble(System.console().readLine());
+		System.out.print("Nota 2: ");
+		Double nota2 = Double.parseDouble(System.console().readLine());
+		System.out.print("Nota 3: ");
+		Double nota3 = Double.parseDouble(System.console().readLine());
+
+		double notaMedia = ((nota1 + nota2 + nota3)/ 3);
+		System.out.print("La nota media es " + notaMedia);
+ }
+}
