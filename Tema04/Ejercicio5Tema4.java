b898137d4daa020fc126919f302c824deb962439
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-21 15:51:15 +0200

    Ejercicio5

---
+++ Tema04/Ejercicio5Tema4.java
@@ -0,0 +1,19 @@
+/**
+* 	Realiza un programa que resuelva una ecuación de primer grado (del tipo ax+b = 0).
+*
+* @author Alberto Parra 
+*/
+public class Ejercicio5Tema4 {
+	public static void main(String[] args) {
+		System.out.print("Valor de X: ");
+		Double X = Double.parseDouble(System.console().readLine());
+		System.out.print("valor de Y: ");
+		Double Y = Double.parseDouble(System.console().readLine());
+
+		if (X == 0) {
+		System.out.println("Esa ecuación no tiene solución real.");
+		} else {
+		System.out.println("x = " + (-Y/X));
+		}
+ }
+}
