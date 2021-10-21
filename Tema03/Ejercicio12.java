9704def2b5e90ec3005ce0b0b53ab46058f1ee04
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-21 08:58:18 +0200

    Ejercicio12.java

--- Tema03/Ejercicio12.java
+++ Tema03/Ejercicio12.java
@@ -17,19 +17,19 @@
 * @author Alberto Parra 
 */
 public class Ejercicio12 {
-public static void main(String[] args) {
-	System.out.println("Problemas");
+	public static void main(String[] args) {
+		System.out.println("Problemas");
 	
-	//Introducimos la nota del primer examen
-	System.out.print("Introduce la nota del primer examen: ");
-    double examen1 = Double.parseDouble(System.console().readLine());
+		//Introducimos la nota del primer examen
+		System.out.print("Introduce la nota del primer examen: ");
+		double examen1 = Double.parseDouble(System.console().readLine());
 
-    //La nota que deseamos
-    System.out.print("¿Qué nota quieres sacar en el trimestre? ");
-    double notaFinal = Double.parseDouble(System.console().readLine());
+		//La nota que deseamos
+		System.out.print("¿Qué nota quieres sacar en el trimestre? ");
+		double notaFinal = Double.parseDouble(System.console().readLine());
     
-	//La factorización del examen1 con la notaFinal deseada.
-	double examen2 = ((notaFinal * 100) - (examen1 * 40)) / 60;
-    System.out.println("Para poder tener un " + notaFinal + " en el trimestre, necesitas sacar al menos  " + examen2 + " en el segundo examen.");	    
+		//La factorización del examen1 con la notaFinal deseada.
+		double examen2 = ((notaFinal * 100) - (examen1 * 40)) / 60;
+		System.out.println("Para poder tener un " + notaFinal + " en el trimestre, necesitas sacar al menos  " + examen2 + " en el segundo examen.");	    
  }
 }
