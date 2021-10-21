e49915db959723cb508e2484756cc3c16a5ef8ef
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-21 10:56:21 +0200

    Ejercicio3

---
+++ Tema04/Ejercicio3Tema4.java
@@ -0,0 +1,50 @@
+/**
+ * Escribe un programa que pida por teclado un día de la semana y que diga qué
+   asignatura toca a primera hora ese día.
+ *
+ * @author Alberto Parra
+ */
+public class Ejercicio3Tema4 {
+  public static void main(String[] args) {   
+    
+    System.out.print("Por favor, introduzca un numero de la semana: ");
+	int dia = Integer.parseInt(System.console().readLine());
+	String nombreDelDia;
+	
+	switch (dia) {
+		case 1:
+		nombreDelDia = "Lunes";		
+		break;
+		
+		case 2:
+		nombreDelDia = "Martes";		
+		break;
+		
+		case 3:
+		nombreDelDia = "Miércoles";		
+		break;
+		
+		case 4:
+		nombreDelDia = "Jueves";		
+		break;
+		
+		case 5:
+		nombreDelDia = "Viernes";		
+		break;
+		
+		case 6:
+		nombreDelDia = "Sábado";		
+		break;
+		
+		case 7:
+		nombreDelDia = "Domingo";		
+		break;	
+		
+		default :
+		nombreDelDia = "No existe ese día";		
+		break;
+		}
+    
+    System.out.println("Día " + dia + ": " + nombreDelDia);    
+  }
+}
