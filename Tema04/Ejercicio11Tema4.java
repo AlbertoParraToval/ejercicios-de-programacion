c8b1711a5c62506bae40b59ac5f9b34334d4fb82
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-23 18:10:25 +0200

    Ejercicio11

---
+++ Tema04/Ejercicio11Tema4.java
@@ -0,0 +1,14 @@
+/**
+* 	Realiza un programa que resuelva una ecuación de primer grado (del tipo ax+b = 0).
+*
+* @author Alberto Parra 
+*/
+public class Ejercicio11Tema4 {
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+ }
+}
