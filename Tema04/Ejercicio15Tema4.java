321a0464ab631a020765492b413da5ba4d8410b8
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-24 12:22:25 +0200

    Ejercicio15

---
+++ Tema04/Ejercicio15Tema4.java
@@ -0,0 +1,22 @@
+/**
+* 	Escribe un programa que pinte una pirámide rellena con un carácter introducido
+	por teclado que podrá ser una letra, un número o un símbolo como *, +, -, $, &,
+	etc. El programa debe permitir al usuario mediante un menú elegir si el vértice
+	de la pirámide está apuntando hacia arriba, hacia abajo, hacia la izquierda o
+	hacia la derecha
+* @author Alberto Parra 
+*/
+public class Ejercicio15Tema4 {
+	public static void main(String[] args) {
+		
+		System.out.print("Introduce la letra o el simbolo para formar tu pirámide ");
+		int formato = Integer.parseInt(System.console().readLine());
+		
+		System.out.print("Introduce  el lugar del vertice de tu pirámide");
+		int vertice = Integer.parseInt(System.console().readLine());
+		
+		
+		
+	
+  }
+}
