9c7d4d7b8de63d58f4ea467b473cbda009400029
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-25 19:19:57 +0200

    Piramide

--- Tema04/Ejercicio15Tema4.java
+++ Tema04/Ejercicio15Tema4.java
@@ -7,16 +7,44 @@
 * @author Alberto Parra 
 */
 public class Ejercicio15Tema4 {
-	public static void main(String[] args) {
-		
-		System.out.print("Introduce la letra o el simbolo para formar tu pirámide ");
-		int formato = Integer.parseInt(System.console().readLine());
-		
-		System.out.print("Introduce  el lugar del vertice de tu pirámide");
-		int vertice = Integer.parseInt(System.console().readLine());
-		
-		
-		
-	
+
+  public static void main(String[] args) {
+        
+    System.out.print("Introduzca el caracter para rellenar tu piramide: ");
+    String c = System.console().readLine();
+    System.out.println("Elija un tipo de pirámide");
+    System.out.println("1. Vértice hacia arriba");
+    System.out.println("2. Vértice hacia abajo");
+    System.out.println("3. Vértice hacia la izquierda");
+    System.out.println("4. Vértice hacia la derecha");
+    int formato = Integer.parseInt(System.console().readLine());
+
+    switch(formato) {
+      case 1:
+        System.out.println("  " + c);
+        System.out.println(" " + c + c + c);
+        System.out.println(c + c + c + c + c);
+        break;
+      case 2:
+        System.out.println(c + c + c + c + c);
+        System.out.println(" " + c + c + c);
+        System.out.println("  " + c);
+        break;
+      case 3:
+        System.out.println("    " + c);
+        System.out.println("  " + c + " " + c);
+        System.out.println(c + " " + c + " " + c);
+        System.out.println("  " + c + " " + c);
+        System.out.println("    " + c);
+        break;
+      case 4:
+        System.out.println(c);
+        System.out.println(c + " " + c);
+        System.out.println(c + " " + c + " " + c);
+        System.out.println(c + " " + c);
+        System.out.println(c);
+        break;
+      default:
+    }
   }
 }
