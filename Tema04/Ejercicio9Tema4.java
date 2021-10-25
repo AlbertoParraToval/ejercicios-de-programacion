a68f92045ac0724eaf9de3393348461bd8f8a31d
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-25 12:52:36 +0200

    Ecuacion Segundo grado

--- Tema04/Ejercicio9Tema4.java
+++ Tema04/Ejercicio9Tema4.java
@@ -14,18 +14,47 @@ public class Ejercicio9Tema4 {
     
     System.out.println("Por favor, introduzca los valores.");
     
-    System.out.print("x = ");
-    double x = Double.parseDouble(System.console().readLine());
+    System.out.print("a = ");
+    double a = Double.parseDouble(System.console().readLine());
     
-    System.out.print("y = ");
-    double y = Double.parseDouble(System.console().readLine());
+    System.out.print("b = ");
+    double b = Double.parseDouble(System.console().readLine());
     
     System.out.print("c = ");
     double c = Double.parseDouble(System.console().readLine());
-   
+    
+    if ((a == 0) && (b == 0) && (c == 0)) {
+      System.out.println("La ecuación tiene infinitas soluciones.");
+    }
     
     
+    if ((a == 0) && (b == 0) && (c != 0)) {
+      System.out.println("La ecuación no tiene solución.");
+    }
+      
+    
+    if ((a != 0) && (b != 0) && (c == 0)) {
+      System.out.println("x1 = 0");
+      System.out.println("x2 = " + (-b / a));
+    }
+
+    
+    if ((a == 0) && (b != 0) && (c != 0)) {
+      System.out.println("x1 = x2 = " + (-c / b));
+    }
+
+    
+    if ((a != 0) && (b != 0) && (c != 0)) {
+        
+      double discriminante = b*b - (4 * a * c);
+      
+      if (discriminante < 0) {
+          System.out.println("La ecuación no tiene soluciones reales");
+      }  else {
+        System.out.println("x1 = " + (-b + Math.sqrt(discriminante))/(2 * a));
+        System.out.println("x2 = " + (-b - Math.sqrt(discriminante))/(2 * a));
       }
     }
+
   }
 }
