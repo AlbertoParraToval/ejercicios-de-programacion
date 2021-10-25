127fca1b373f3d569cab09c0ed5efb3cebf1564c
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-25 12:47:25 +0200

    Salario2.0

--- Tema04/Ejercicio4tema4.java
+++ Tema04/Ejercicio4tema4.java
@@ -1,6 +1,5 @@
 /**
-* 	Vamos a ampliar uno de los ejercicios de la relación anterior para considerar
-	las horas extras. Escribe un programa que calcule el salario semanal de un
+* 	Escribe un programa que calcule el salario semanal de un
 	trabajador teniendo en cuenta que las horas ordinarias (40 primeras horas de
 	trabajo) se pagan a 12 euros la hora. A partir de la hora 41, se pagan a 16 euros
 	la hora.
