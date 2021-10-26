6020cf23cb8ff1759262386706ae6737b769703a
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-10-26 10:54:56 +0200

    Nota Media 3.0

--- Tema04/Ejercicio20Tema4.java
+++ Tema04/Ejercicio20Tema4.java
@@ -3,36 +3,30 @@
 	teclado es capicúa. Se permiten números de hasta 5 cifras.
 * @author Alberto Parra 
 */
-public class Ejercicio20Tema4 {
-	public static void main(String[] args) {
-		
-		System.out.print("Introduce el número cuya primera cifra quiera saber:\n ");
-		int numero = Integer.parseInt(System.console().readLine());
 
-		//Si es mayor de 5 no se cuenta
-		if (numero > 99999) {
-			System.out.printf("No se pueden los números de más de 5 cifras.");
-			}
-		//Si el numero tiene 5 cifras
-		if ((numero <= 99999) && (numero >= 10000)) {
-			System.out.printf("La primera cifra es " + (numero / 10000));
-			}
-			//Si el numero tiene 4 cifras
-		if ((numero <= 9999) && (numero >= 1000)) {
-			System.out.printf("La primera cifra es " + (numero / 1000));
-			}
-			//Si el numero tiene 3 cifras
-		if ((numero <= 999) && (numero >= 100)) {
-			System.out.printf("La primera cifra es " + (numero / 100));
-			}
-			//Si el numero tiene 2 cifras
-		if ((numero <= 99) && (numero >= 10) ) {
-			System.out.printf("La primera cifra es " + (numero / 10));
-			}
-			//Si el numero tiene 1 cifra
-		if ((numero <= 9) && (numero >= 0)) {
-			System.out.printf("La primera cifra es " + numero);
-			}
 		
+public class Ejercicio20Tema4 {
+  public static void main (String[] args) {
+     String line;
+      System.out.print(" Escribe un número entero positivo: ");
+      line = System.console().readLine();
+      int num, inv = 0, cifra;
+      int aux = 0;
+      num = Integer.parseInt(line);
+      //invertir número
+      num = aux;
+      cifra = aux % 10; 
+      inv = inv * 10 + cifra;
+      aux = aux / 10;
+      if(num == inv){
+            System.out.println(" El número es capicua");
+      }else {
+		System.out.println(" El número no es capicua");
+    }
   }
 }
+ 
+
+
+
+		
