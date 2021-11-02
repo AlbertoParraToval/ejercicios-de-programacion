9d085140f4b678e46e9ada08044a38b02bfcfe6b
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-11-02 09:33:27 +0100

    Cuestionario

--- Tema04/Ejercicio12Tema4.java
+++ Tema04/Ejercicio12Tema4.java
@@ -16,7 +16,7 @@ public class Ejercicio12Tema4 {
 		//Pregunta 1
 		System.out.println("1. ¿Cuál de los siguientes tipos de datos de Java tiene más precisión?");
 		System.out.println("a) int\nb) double\nc) float"); 
-		System.out.print("---->>> ");
+		System.out.print("Mi respuesta es la ");
 		respuesta = System.console().readLine();
 		if (respuesta.equals("b")) {
 		puntos++;
@@ -26,7 +26,7 @@ public class Ejercicio12Tema4 {
 		//Pregunta 2
 		System.out.println("2. ¿Cuales es el modelo de software más básico y sencillo?");
 		System.out.println("a) Waterfall\nb) Evolution Prototype\nc) Spiral"); 
-		System.out.print("---->>> ");
+		System.out.print("Mi respuesta es la ");
 		respuesta = System.console().readLine();
 		if (respuesta.equals("a")) {
 		puntos++;
@@ -35,7 +35,7 @@ public class Ejercicio12Tema4 {
 		//Pregunta 3
 		System.out.println("3. El sindrome del dedo blanco es debido a:");
 		System.out.println("a) Agentes físicos\nb) Agentes Biológicos\nc) Enfermedades profesionales de la piel"); 
-		System.out.print("---->>> ");
+		System.out.print("Mi respuesta es la ");
 		respuesta = System.console().readLine();
 		if (respuesta.equals("a")) {
 		puntos++;
@@ -44,7 +44,7 @@ public class Ejercicio12Tema4 {
 		//Pregunta 4
 		System.out.println("4. Pasa este número hexadecimal a octal: C127");
 		System.out.println("a) 234457\nb) 150557\nc) 152392"); 
-		System.out.print("---->>> ");
+		System.out.print("Mi respuesta es la ");
 		respuesta = System.console().readLine();
 		if (respuesta.equals("b")) {
 		puntos++;
@@ -53,7 +53,7 @@ public class Ejercicio12Tema4 {
 		//Pregunta 5
 		System.out.println("5. ¿Cuántos tipos de organización de ficheros existen?");
 		System.out.println("a) 3\nb) 4\nc) 2"); 
-		System.out.print("---->>> ");
+		System.out.print("Mi respuesta es la ");
 		respuesta = System.console().readLine();
 		if (respuesta.equals("a")) {
 		puntos++;
@@ -62,7 +62,7 @@ public class Ejercicio12Tema4 {
 		//Pregunta 6
 		System.out.println("6. ¿Cómo se divide el diseño de una base de datos?");
 		System.out.println("a) Diseño Conceptual, Lógico y Físico.\nb) Diseño Conceptual, Físico y Lógico.\nc) Diseño Lógico, Conceptual y Físico."); 
-		System.out.print("---->>> ");
+		System.out.print("Mi respuesta es la ");
 		respuesta = System.console().readLine();
 		if (respuesta.equals("b")) {
 		puntos++;
@@ -71,7 +71,7 @@ public class Ejercicio12Tema4 {
 		//Pregunta 7
 		System.out.println("7. ¿Qué es un riesgo laboral?");
 		System.out.println("a) El daño que te haces en el trabajo\nb) Las posibilidades de verse en un accidente debido al cumplimiento de tus funciones de trabajo\nc) Todos los accidentes de trabajo"); 
-		System.out.print("---->>> ");
+		System.out.print("Mi respuesta es la ");
 		respuesta = System.console().readLine();
 		if (respuesta.equals("b")) {
 		puntos++;
@@ -80,7 +80,7 @@ public class Ejercicio12Tema4 {
 		//Pregunta 8
 		System.out.println("8. ¿Qué es el software?");
 		System.out.println("a) La parte física del ordenador\nb) Una invención de la gente, no existe realmente\nc) Conjunto de código informático que se apoya en el hardware."); 
-		System.out.print("---->>> ");
+		System.out.print("Mi respuesta es la ");
 		respuesta = System.console().readLine();
 		if (respuesta.equals("c")) {
 		puntos++;
@@ -89,7 +89,7 @@ public class Ejercicio12Tema4 {
 		//Pregunta 9
 		System.out.println("9. ¿Cúantos números tiene el sistema hexadecimal?");
 		System.out.println("a) 16\nb) 8\nc) 6"); 
-		System.out.print("---->>> ");
+		System.out.print("Mi respuesta es la ");
 		respuesta = System.console().readLine();
 		if (respuesta.equals("b")) {
 		puntos++;
@@ -98,7 +98,7 @@ public class Ejercicio12Tema4 {
 		//Pregunta 10
 		System.out.println("10. Se pueden clasificar los daños a la salud de los trabajadores en:");
 		System.out.println("a) Enfermedades y patologías\nb) Patologías o lesiones\nc) Patologías específicas o inespecíficas"); 
-		System.out.print("---->>> ");
+		System.out.print("Mi respuesta es la ");
 		respuesta = System.console().readLine();
 		if (respuesta.equals("c")) {
 		puntos++;
