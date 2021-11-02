23feba93f4d10fbcb68e4e818c89ae6c99914454
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-11-02 09:10:19 +0100

    Tipo Char

--- Tema02/Ejercicio8.java
+++ Tema02/Ejercicio8.java
@@ -1,5 +1,7 @@
 /**
- * Ejercicio 8
+ * Escribe un programa que declare 5 variables de tipo char. A continuación, crea
+	otra variable como cadena de caracteres y asígnale como valor la concatenación de las anteriores 5 variables. Por último, muestra la cadena de caracteres
+	por pantalla ¿Qué problemas te encuentras? ¿cómo lo has solucionado?
  *
  * @author Alberto Parra Toval
  */
