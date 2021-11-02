daa2d9273aa238092306f4165657861069dc85f2
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-11-02 09:09:24 +0100

    Tipo char y String

--- Tema02/Ejercicio7.java
+++ Tema02/Ejercicio7.java
@@ -1,5 +1,7 @@
 /**
- * Ejercicio 7
+ * Escribe un programa que declare variables de tipo char y de tipo String. Intenta
+  mostrarlas por pantalla todas juntas en la misma línea y con una sola sentencia
+  de Java (con un solo println) ¿es posible
  *
  * @author Alberto Parra Toval
  */
@@ -8,8 +10,7 @@ public class Ejercicio7 {
 
 	char texto2 = 'N';
 	char texto3 = 'o';
-	
-
+  
     String texto1 = "me gustan las peleas de boxeo";
 
     System.out.println(texto2 + "" + texto3 + " " + texto1);
