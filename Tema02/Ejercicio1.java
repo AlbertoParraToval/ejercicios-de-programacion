5ae79a726db7f96cedeace036f2df85e8e4364cc
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-11-02 09:02:45 +0100

    Me lllamo ...

--- Tema02/Ejercicio1.java
+++ Tema02/Ejercicio1.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Escribe un programa en el que se declaren las variables enteras x e y. Asígnales
   los valores 144 y 999 respectivamente. A continuación, muestra por pantalla
   el valor de cada variable, la suma, la resta, la división y la multiplicación.
--- Tema02/Ejercicio2.java
+++ Tema02/Ejercicio2.java
@@ -1,5 +1,7 @@
 /**
- * Ejercicio 2
+ *Crea la variable nombre y asígnale tu nombre completo. Muestra su valor por
+  pantalla de tal forma que el resultado del programa sea el mismo que en el
+  ejercicio 1 del capítulo 1.
  *
  * @author Alberto Parra Toval
  */
