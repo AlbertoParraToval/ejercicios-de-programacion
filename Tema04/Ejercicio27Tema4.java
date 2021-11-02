1bb10e0c24e20872c2cf951bd4f19fb02ac3708d
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-11-02 08:18:49 +0100

    Presupuesto Tartas

---
+++ Tema04/Ejercicio27Tema4.java
@@ -0,0 +1,9 @@
+/*
+ * Una pastelería nos ha pedido realizar un programa que haga presupuestos de
+	tartas. El programa preguntará primero de qué sabor quiere el usuario la tarta:
+	manzana, fresa o chocolate. La tarta de manzana vale 18 euros y la de fresa
+	16. En caso de seleccionar la tarta de chocolate, el programa debe preguntar
+	además si el chocolate es negro o blanco; la primera opción vale 14 euros y la
+	segunda 15. Por último se pregunta si se añade nata y si se personaliza con un
+	nombre; la nata suma 2.50 y la escritura del nombre 2.75.
+*/
