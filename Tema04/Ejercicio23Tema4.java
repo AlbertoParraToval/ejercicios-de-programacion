ab128f37649b99903abe067da9fc191c012e9760
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-11-02 19:18:23 +0100

    Base Imponible

--- Tema04/Ejercicio23Tema4.java
+++ Tema04/Ejercicio23Tema4.java
@@ -10,23 +10,75 @@
 * @author Alberto Parra 
 */
 public class Ejercicio23Tema4 {
-  public static void main (String[] args) {
-     String line;
-      System.out.print(" Escribe un número entero positivo: ");
-      line = System.console().readLine();
-      int num, inv = 0, cifra;
-      int aux = 0;
-      num = Integer.parseInt(line);
-      //invertir número
-      num = aux;
-      cifra = aux % 10; 
-      inv = inv * 10 + cifra;
-      aux = aux / 10;
-      if(num == inv){
-            System.out.println(" El número es capicua");
-      }else {
-		System.out.println(" El número no es capicua");
+      public static void main(String[] args) {
+        boolean precioValido = true;
+
+        System.out.println("Vamos a calcular el precio final de un producto: ");
+        System.out.print("Primero dime cuál es la base imponible en euros: ");
+        double baseImp = Double.parseDouble(System.console().readLine());
+    
+        System.out.print("\nAhora dime cuál es el IVA, puede ser... ");
+        System.out.println("general: 21% (escribe 1),  reducido: 10% (escribe 2), o superreducido: 4% (escribe 3)");
+        int iva = Integer.parseInt(System.console().readLine());
+    
+        System.out.println("¿Tienes algún código promocional? (s/n): ");
+        String aux = System.console().readLine();
+        String codigo = "";
+    
+    
+        if(baseImp < 0){
+          precioValido = false;
+        }
+        double precio = baseImp;
+        //Tipo de IVA
+        switch (iva) {
+        case 1:
+          precio = precio * 1.21;
+          break;
+    
+        case 2:
+          precio = precio * 1.1;
+          break;
+    
+        case 3:
+          precio = precio * 1.04;
+          break;
+    
+        default:
+          precioValido = false;
+        }
+    
+        if (aux.equals("s")) {
+          System.out.println("Introduce tu código por favor: ");
+          codigo = System.console().readLine();
+    
+          //Tipo de Código
+          switch(codigo){
+    
+            case "mitad":
+              precio = precio / 2;
+              break;
+        
+            case "menos5":
+              precio = precio - 5;
+              break;
+        
+            case "5porc":
+              precio -= precio * 0.05;
+              break;
+        
+            default:
+              precioValido = false;
+            break;
+            }
+    
+        }
+
+        if(precioValido == true){
+          System.out.printf("El precio final es igual a: %.2f€ \n", precio);
+        } else{
+          System.out.println("Hay algún dato mal introducido, por favor, inténtalo de nuevo");
+        }
+    
+      }
     }
-  }
-}
- 
