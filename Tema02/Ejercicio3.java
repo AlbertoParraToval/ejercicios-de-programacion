3291dd48d5eb99886b3693b2e751a005f0545178
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-11-02 09:03:37 +0100

    Tfno, nombre, dirección

--- Tema02/Ejercicio3.java
+++ Tema02/Ejercicio3.java
@@ -1,5 +1,6 @@
 /**
- * Ejercicio 3
+ * Crea las variables nombre, direccion y telefono y asígnales los valores correspondientes. Muestra los valores de esas variables por pantalla de tal forma que el
+  resultado del programa sea el mismo que en el ejercicio 2
  *
  * @author Alberto Parra Toval
  */
