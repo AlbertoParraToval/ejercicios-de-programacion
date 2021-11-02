3efa638c29fab6100e453344a4e37ef3617342b6
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-11-02 09:19:56 +0100

    Sueldo Semanal

--- Tema03/Ejercicio8.java
+++ Tema03/Ejercicio8.java
@@ -2,6 +2,7 @@
 * Escribe un programa que calcule el salario semanal de un empleado en base a
   las horas trabajadas, a razón de 12 euros la hora.
 *
+*
 * @author Alberto Parra 
 */
 public class Ejercicio8 {
