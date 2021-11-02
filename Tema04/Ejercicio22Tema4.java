6de5112f287910cf8c3a546d0d158cd9ffd4498b
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-11-02 08:50:38 +0100

    Cálculo hasta el finde semana

--- Tema04/Ejercicio22Tema4.java
+++ Tema04/Ejercicio22Tema4.java
@@ -1,11 +1,14 @@
 /**
-* 	Realiza un programa que diga si un número entero positivo introducido por
-	teclado es capicúa. Se permiten números de hasta 5 cifras.
+* 	Realiza un programa que, dado un día de la semana (de lunes a viernes) y una
+  hora (horas y minutos), calcule cuántos minutos faltan para el fin de semana.
+  Se considerará que el fin de semana comienza el viernes a las 15:00h. Se da
+   hecho que el usuario introducirá un día y hora correctos, anterior al viernes
+  a las 15:00h.
 * @author Alberto Parra 
 */
 
 		
-public class Ejercicio20Tema4 {
+public class Ejercicio22Tema4 {
   public static void main (String[] args) {
      
      
--- Tema04/Ejercicio23Tema4.java
+++ Tema04/Ejercicio23Tema4.java
@@ -1,11 +1,18 @@
 /**
-* 	Realiza un programa que diga si un número entero positivo introducido por
-	teclado es capicúa. Se permiten números de hasta 5 cifras.
+* 	Escribe un programa que calcule el precio final de un producto según su
+  base imponible (precio antes de impuestos), el tipo de IVA aplicado (general,
+  reducido o superreducido) y el código promocional. Los tipos de IVA general,
+  reducido y superreducido son del 21%, 10% y 4% respectivamente. Los códigos
+  promocionales pueden ser nopro, mitad, meno5 o 5porc que significan
+  respectivamente que no se aplica promoción, el precio se reduce a la mitad,
+  se descuentan 5 euros o se descuenta el 5%. El ejercicio se da por bueno si se
+  muestran los valores correctos, aunque los números no estén tabulados.
+
 * @author Alberto Parra 
 */
 
 		
-public class Ejercicio20Tema4 {
+public class Ejercicio23Tema4 {
   public static void main (String[] args) {
      String line;
       System.out.print(" Escribe un número entero positivo: ");
