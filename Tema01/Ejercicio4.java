13ad823821a57d493833e9e39060ea77ec3ce72e
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-11-03 08:32:00 +0100

    Horario

---
+++ Tema01/Ejercicio4.java
@@ -0,0 +1,12 @@
+public class Ejercicio4 { // Clase principal
+  public static void main(String[] args) {
+    System.out.println("Lunes\tMartes\tMier\tJueves\tViernes");
+    System.out.println("======\t======\t======\t======\t=======");
+    System.out.println("Prog\tProg\tSist\tProg\tSist");
+    System.out.println("Prog\tProg\tSist\tProg\tSist");
+    System.out.println("Leng\tProg\tSist\tProg\tSist");
+    System.out.println("Entor\tBases\tLeng\tBases\tFol");
+    System.out.println("Entor\tBases\tLeng\tBases\tFol");
+    System.out.println("Entor\tBases\tLeng\tBases\tFol");
+  }
+}
