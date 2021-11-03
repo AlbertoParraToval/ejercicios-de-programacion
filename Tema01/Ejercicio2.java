0551ddf9c49422dbed792990c6ace6de800ccd6a
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-11-03 08:30:22 +0100

    Nombre, direccion y numero

---
+++ Tema01/Ejercicio2.java
@@ -0,0 +1,7 @@
+public class Ejercicio2 { // Clase principal
+  public static void main(String[] args) {
+    System.out.println("Alberto Parra");
+    System.out.printf("%d \n", 665879313);
+    System.out.println("Avenida Imperio Argentina numero 7");
+  }
+}
