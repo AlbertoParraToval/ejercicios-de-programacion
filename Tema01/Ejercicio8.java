d6bf34d7250c9630f7dafd9fd97b0f3c4ea84111
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-11-03 08:35:06 +0100

    Piramide 3.0

---
+++ Tema01/Ejercicio8.java
@@ -0,0 +1,9 @@
+public class Ejercicio8 { // Clase principal
+  public static void main(String[] args) {
+    System.out.println(" *********");
+    System.out.println("  *     *");
+    System.out.println("   *   *");
+    System.out.println("    * *");
+    System.out.println("     *");
+  }
+}
