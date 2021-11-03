8b3674db129fe06e6f42adce0b040b59f130f3bb
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-11-03 08:36:03 +0100

    Imagen de figura

---
+++ Tema01/Ejercicio9.java
@@ -0,0 +1,14 @@
+public class Ejercicio9 { // Clase principal
+  public static void main(String[] args) {
+    System.out.println("          ███□□□□□□□□□█████████████████████████████");
+    System.out.println("          ███□□□□□□□□□█████████████████████████████");
+    System.out.println("          ███□□□███████□□□□□□□□██████Alberto Parra█");
+    System.out.println("          ███□□□███████□□□  □□□████████████████████");
+    System.out.println("          ███□□□□□□█████████□□□████████████████████");
+    System.out.println("          ███□□□████████████□□□████████████████████");
+    System.out.println("          ███□□□████████████□□□████████████████████");
+    System.out.println("          █□□□□□□□████████  □□□████████████████████");
+    System.out.println("          █□□□□□□□██████████□□□████████████████████");
+
+  }
+}
