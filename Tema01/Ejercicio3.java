ea5a19b5ad4d53206e391aaa29be89e5afc73301
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-11-03 08:31:17 +0100

    Tabla español-ingles

---
+++ Tema01/Ejercicio3.java
@@ -0,0 +1,14 @@
+public class Ejercicio3 {
+  public static void main(String[] args) {
+    System.out.printf("%-10s %s\n", "computer", "ordenador");
+    System.out.printf("%-10s %s\n", "student", "alumno");
+    System.out.printf("%-10s %s\n", "cat", "gato");
+    System.out.printf("%-10s %s\n", "penguin", "pingüino");
+    System.out.printf("%-10s %s\n", "machine", "máquina");
+    System.out.printf("%-10s %s\n", "nature", "naturaleza");
+    System.out.printf("%-10s %s\n", "light", "luz");
+    System.out.printf("%-10s %s\n", "green", "verde");
+    System.out.printf("%-10s %s\n", "book", "libro");
+    System.out.printf("%-10s %s\n", "pyramid", "pirámide");
+  }
+}
