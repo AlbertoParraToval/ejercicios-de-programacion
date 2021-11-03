a1a5f8886850ee6ad93446bee8b5da6c1cfa6e13
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-11-03 08:34:38 +0100

    Piramide 2.0

---
+++ Tema01/Ejercicio7.java
@@ -0,0 +1,9 @@
+public class Ejercicio7 { // Clase principal
+  public static void main(String[] args) {
+    System.out.println("     *");
+    System.out.println("    * *");
+    System.out.println("   *   *");
+    System.out.println("  *     *");
+    System.out.println(" *********");
+  }
+}
