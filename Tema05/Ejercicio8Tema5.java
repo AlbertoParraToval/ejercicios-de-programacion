250fad0f05b696fcfdd5acef31b351c0cf5d6b62
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-11-09 08:41:56 +0100

    Tablas multiplicar

--- Tema05/Ejercicio8Tema5.java
+++ Tema05/Ejercicio8Tema5.java
@@ -9,11 +9,11 @@ public class Ejercicio8Tema5 {
       System.out.println("Introduce el número que desees: ");
       int num = Integer.parseInt(System.console().readLine());
       System.out.println("[La tabla del " + num + "]");
-      System.out.println("--------");
+      System.out.println("---------");
       //Aplicamos el for   
       for (int mult = 1; mult < 11 ; mult++){    
       System.out.println(num + "x" + mult + " = " + num * mult);
-      System.out.println("--------");
+      System.out.println("---------");
       }
   }
 }
