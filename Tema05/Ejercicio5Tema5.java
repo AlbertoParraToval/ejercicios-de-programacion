fbfd8c565599a21de63d93190bb9037b2585ac56
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-11-09 09:20:44 +0100

    Del 320 al 160 (While)

---
+++ Tema05/Ejercicio5Tema5.java
@@ -0,0 +1,18 @@
+/**
+ *  Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizando
+    un bucle while.
+ *
+ * @author Alberto Parra Toval
+ */
+public class Ejercicio5Tema5 {
+    public static void main(String[] args) {
+      int n = 320;
+      while ( n > 159) {    
+      n -= 20;
+      System.out.println("n: " + n);
+      System.out.println("--------");
+      }
+      //Se ejecuta una vez fuera de la condición impuesta por el bucle
+      System.out.println("Fin");
+  }
+}
