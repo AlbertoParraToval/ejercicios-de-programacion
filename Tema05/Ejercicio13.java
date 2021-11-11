1f6afa5b34b694a63cbaee5eaf24de12616252b8
Alberto Parra Toval <alberto.parra.toval@gmail.com>
2021-11-11 10:29:50 +0100

    Numeros Nçegativos y Positivos

--- Tema05/Ejercicio13.java
+++ Tema05/Ejercicio13.java
@@ -9,17 +9,16 @@ public class Ejercicio13 {
     public static void main(String[] args) {
       System.out.println("Programa que lee diez numeros y determine los negativos/positivos");
       System.out.println("Introduce diez números: ");
-      int num = Integer.parseInt(System.console().readLine());
-      
-      int negativos;
-      int positivos;
+      int negativos = 0;
+      int positivos = 0;
 
-      for (int i =0; i < 10; i++) {
-        
+      for (int numeroMax = 0; numeroMax < 10; numeroMax++) {
+        if ( Integer.parseInt(System.console().readLine()) > 0) {
+          positivos++;
+        } else {
+          negativos++;
+        }
       }
-
-
-
-
+      System.out.println("Los numeros positivos son " + positivos + " y los números negativos son " + negativos);
   }
 }
