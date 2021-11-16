fc0e6e4d495d384e8985b1685b72dfe6e5451815
alberto.parra.toval@gmail.com <alberto.parra.toval@gmail.com>
2021-11-16 10:01:05 +0100

    Ejercicio22

---
+++ Tema05/Ejercicio22.java
@@ -0,0 +1,25 @@
+/**
+ * Muestra por pantalla todos los números primos entre 2 y 100, ambos incluidos.
+ * @author Alberto Parra Toval
+ */
+public class Ejercicio22 {
+    public static void main(String[] args) {
+        System.out.println("[Numeros Primos desde el 2 al 100]");
+        System.out.println("-----------------------");
+       
+        int num = 2;
+        int contadorPrimos = 0;
+        boolean primo;
+        boolean noPrimo;
+        for (int i = 0; i < 101; i++) {
+            i++;
+
+            if (primo = true) {
+                contadorPrimos++;
+            }
+
+
+
+        }
+  }
+}
