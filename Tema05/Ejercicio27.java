5c4d7eea6c6fb3676152f4d6000b709ef4655e76
alberto.parra.toval@gmail.com <alberto.parra.toval@gmail.com>
2021-11-18 09:24:38 +0100

    En progreso

--- Tema05/Ejercicio27.java
+++ Tema05/Ejercicio27.java
@@ -1,31 +1,27 @@
-public class Ejercicio24 {
-    public static void main(String[] args) {
-        System.out.println("[Piramide]");
-        System.out.println("--------------");
-        System.out.print("Introduzca la altura de la pirámide: ");
-        int alturaIntroducida = Integer.parseInt(System.console().readLine());
+/**
+ *  Escribe un programa que muestre, cuente y sume los múltiplos de 3 que hay entre 1 y un número leído por teclado
+ * 
+ * @author Alberto Parra Toval
+ */
+public class Ejercicio27 {
+  public static void main(String[] args) {
+    System.out.println("Ejercicio que muestra las suma de 100 numeros a partir de uno");
+    int numeroIntroducido = 0;
+
+    do {
+        System.out.print("Introduzca un numero entero positivo: ");
+        numeroIntroducido = Integer.parseInt(System.console().readLine());
     
-        System.out.print("Introduzca el carácter de relleno: ");
-        int caracter = Integer.parseInt(System.console().readLine());;
-        
-        int base = 1;
-        int longitud = 1;
-        int espacios = alturaIntroducida-1;
-        
-        while (base <= alturaIntroducida) {
-        
-          for (int i = 1; i <= espacios; i++) {
-            System.out.print(" ");
-          }
-    
-          for (int i = 1; i <= longitud; i++) {
-            System.out.print(caracter);
-          }
-    
-          System.out.println();
-          base++;
-          espacios--;
-          longitud += 2;
+        if(numeroIntroducido < 1) {
+      System.out.println("El número introducido no es correcto.");
         }
+    } while (numeroIntroducido < 1);
+
+    int multiplo = 3;
+    for(int n = numeroIntroducido; n < numeroIntroducido ; n++) {
+      n += multiplo;
+      System.out.println(numeroIntroducido + " ");
     }
+    System.out.println(" ");
+}
 }
