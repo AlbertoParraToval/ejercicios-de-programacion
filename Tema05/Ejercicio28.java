5d9dc8ad59d6a703eeddf0b7e9df3be4a0c5036a
alberto.parra.toval@gmail.com <alberto.parra.toval@gmail.com>
2021-11-20 19:28:22 +0100

    Añadido

--- Tema05/Ejercicio28.java
+++ Tema05/Ejercicio28.java
@@ -1,31 +1,19 @@
 public class Ejercicio24 {
-    public static void main(String[] args) {
-        System.out.println("[Piramide]");
-        System.out.println("--------------");
-        System.out.print("Introduzca la altura de la pirámide: ");
-        int alturaIntroducida = Integer.parseInt(System.console().readLine());
-    
-        System.out.print("Introduzca el carácter de relleno: ");
-        int caracter = Integer.parseInt(System.console().readLine());;
-        
-        int base = 1;
-        int longitud = 1;
-        int espacios = alturaIntroducida-1;
-        
-        while (base <= alturaIntroducida) {
-        
-          for (int i = 1; i <= espacios; i++) {
-            System.out.print(" ");
-          }
-    
-          for (int i = 1; i <= longitud; i++) {
-            System.out.print(caracter);
-          }
-    
-          System.out.println();
-          base++;
-          espacios--;
-          longitud += 2;
-        }
-    }
+  public static void main(String[] args) {
+     for (int i = 0; i < 40; i++) {
+      switch((int)(Math.random()*7)) {
+        case 0:
+          System.out.println("😀");
+          break;
+        case 1:
+          System.out.println("🐱");
+          break;
+        case 2:
+          System.out.println("🌴");
+        default:
+        System.out.println("_");
+        break;
+      }
+    }  
+  }
 }
---
+++ Tema06/Ejercicio1.java
@@ -0,0 +1,10 @@
+/**
+ * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle do-while.
+ *
+ * @author Alberto Parra Toval
+ */
+public class Ejercicio1 {
+    public static void main(String[] args) {
+      
+    }
+}
