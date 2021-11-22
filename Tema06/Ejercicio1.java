422933944c8aab2ca104af4656c64c2dbf9e17e2
alberto.parra.toval@gmail.com <alberto.parra.toval@gmail.com>
2021-11-22 08:37:42 +0100

    Lanzar 3 dados + Suma de ellos

--- Tema06/Ejercicio1.java
+++ Tema06/Ejercicio1.java
@@ -1,10 +1,21 @@
-/**
- * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle do-while.
+/** Escribe un programa que muestre la tirada de tres dados. Se debe mostrar
+también la suma total (los puntos que suman entre los tres dados)
  *
  * @author Alberto Parra Toval
  */
 public class Ejercicio1 {
     public static void main(String[] args) {
-      
+        System.out.println("Programa que lanza 3 dados y suma sus resultados:");
+        int dados;
+        int suma = 0;
+        System.out.print("Tirada de tres dados: ");
+    
+        for(int i = 0; i < 3; i++) {
+          dados = (int)(Math.random() * 6) + 1;
+        System.out.print(dados + " ");
+        suma += dados;
+        }
+    
+        System.out.println("\nLa Suma: " + suma);
     }
 }
