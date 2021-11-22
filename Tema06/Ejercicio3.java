481bcbc98886c808253e4f234ee64ddcc644108d
alberto.parra.toval@gmail.com <alberto.parra.toval@gmail.com>
2021-11-22 09:05:52 +0100

    Baraja Española

--- Tema06/Ejercicio3.java
+++ Tema06/Ejercicio3.java
@@ -1,9 +1,5 @@
-/** Realiza un programa que muestre al azar el nombre de una carta de la baraja
-francesa. Esta baraja está dividida en cuatro palos: picas, corazones, diamantes
-y tréboles. Cada palo está formado por 13 cartas, de las cuales 9 cartas son
-numerales y 4 literales: 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K y A (que sería el
-1). Para convertir un número en una cadena de caracteres podemos usar
-String.valueOf(n).
+/** gual que el ejercicio anterior pero con la baraja española. Se utilizará la baraja
+de 40 cartas: 2, 3, 4, 5, 6, 7, sota, caballo, rey y as.
  *
  * @author Alberto Parra Toval
  */
