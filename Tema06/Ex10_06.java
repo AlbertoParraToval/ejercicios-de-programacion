4ba9343c671a4fbad0ded9f0f80ab8a08ea6e54d
alberto.parra.toval@gmail.com <alberto.parra.toval@gmail.com>
2021-11-23 09:13:53 +0100

    20 Notas aleatorias y su valoración

--- Tema06/Ex10_06.java
+++ Tema06/Ex10_06.java
@@ -1,10 +1,44 @@
-/** Muestra 20 números enteros aleatorios entre 0 y 10 (ambos incluidos) separados por espacios
+import javax.sound.sampled.LineEvent;
+
+/** Realiza un programa que pinte por pantalla diez líneas formadas por caracteres.
+El carácter con el que se pinta cada línea se elige de forma aleatoria entre uno
+de los siguientes: *, -, =, ., |, @. Las líneas deben tener una longitud aleatoria
+entre 1 y 40 caracteres.
+
  *
  * @author Alberto Parra Toval
  */
 public class Ex10_06 {
     public static void main(String[] args) {
+        System.out.println("[Programa que pinta diez lineas formadas por caracteres]");
+        int longitud = 10;
 
-      
+        for (int i = longitud; i <= 40; i++) {
+            i++;
+            int caracter = (int)(Math.random()*5); 
+            switch (caracter) {
+                case 1:
+                System.out.println("@");
+                    break;
+                case 2:
+                System.out.println("*");
+                    break;
+                case 3:
+                System.out.println("=");
+                    break;
+                case 4:
+                System.out.println(".");
+                    break;
+                default:
+                System.out.println("|");
+                    break;
+            }
+        }
+
+
+
+
+
+
     }
 }
--- Tema06/Ex11_06.java
+++ Tema06/Ex11_06.java
@@ -4,7 +4,47 @@
  */
 public class Ex11_06 {
     public static void main(String[] args) {
-    
-      
+        System.out.println("[Programa que muestra 20 notas generadas al azar]");
+        int notas;
+        int numBien = 0;
+        int numNotable = 0;
+        int numSobre = 0;
+        int numSuficientes = 0;
+        int numSuspensos = 0;
+        for (int i = 1; i <= 20; i++) {
+            notas = (int)(Math.random()*9 + 1);
+            switch (notas) {
+                case 1:
+                System.out.println("Sobresaliente");
+                numSobre++;
+                    break;
+
+                case 2:
+                System.out.println("Notable");
+                numNotable++;
+                    break;
+
+                case 3:
+                System.out.println("Bien");
+                numBien++;
+                    break;
+            
+                case 4:
+                System.out.println("Suficiente");
+                numSuficientes++;
+                    break;
+            
+                default:
+                System.out.println("Suspenso");
+                numSuspensos++;
+                    break;
+            }
+        }
+        System.out.println(" ");;
+        System.out.println("Las notas con sobresaliente son: " + numSobre);
+        System.out.println("Las notas con notable son: " + numNotable);
+        System.out.println("Las notas con bien son: " + numBien);
+        System.out.println("Las notas con suficiente " + numSuficientes);
+        System.out.println("Las notas con suspenso " + numSuspensos);
     }
 }
