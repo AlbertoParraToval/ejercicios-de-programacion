1fe69774e5d5e2929bf21a09f67fe04f07aacc9b
Alberto Parra toval <alberto.parra.toval@gmail.com>
2021-11-25 10:40:16 +0100

    U

--- Tema05/Ejercicio33.java
+++ Tema05/Ejercicio33.java
@@ -1,5 +1,24 @@
-public class Ejercicio24 {
+public class Ejercicio33 {
   public static void main(String[] args) {
-    
-  }
+    System.out.println("[Programa que dibuja una U en base a su altura]");
+    System.out.print("Introduce la altura de tu U: ");
+      int alturaIntroducida =  Integer.parseInt(System.console().readLine());
+      int anchura = alturaIntroducida -2 ;
+      int espacios = alturaIntroducida;
+      //Código de los dos palitos
+      for (int i = 1; i < alturaIntroducida; i++) {
+        System.out.print("*");
+        for (int j = 2; j < alturaIntroducida; j++) {
+          System.out.print("  ");
+        }
+        System.out.print(" ");
+        System.out.println("*");
+      }
+      //Código de la base que depende de la altura introducida
+      for (int a = 0; a <= anchura; a++) {
+        System.out.print(" ");
+        System.out.print("*");
+        }
+      
+    }
 }
