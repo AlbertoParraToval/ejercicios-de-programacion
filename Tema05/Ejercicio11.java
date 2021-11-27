c66ff36f382fff381b6146d0a92df574dabf4311
Alberto Parra toval <alberto.parra.toval@gmail.com>
2021-11-27 23:13:14 +0100

    Ejercicio11

--- Tema05/Ejercicio11.java
+++ Tema05/Ejercicio11.java
@@ -1,6 +1,6 @@
 /**
  *  Escribe un programa que muestre en tres columnas, el cuadrado y el cubo de
-    los 5 primeros números enteros a partir de uno que se introduce por teclado.
+    los 5 primeros números enteros a partir de uno que se introduce por teclado
 
  * @author Alberto Parra Toval
  */
--- Tema05/Ejercicio30.java
+++ Tema05/Ejercicio30.java
@@ -1,16 +1,26 @@
 /**
- *  Escribe un programa que obtenga los números enteros comprendidos entre dos
-    números introducidos por teclado y validados como distintos, el programa debe
-    empezar por el menor de los enteros introducidos e ir incrementando de 7 en
-    7.
+ *  Realiza una programa que calcule las horas transcurridas entre dos horas de
+dos días de la semana. No se tendrán en cuenta los minutos ni los segundos.
+El día de la semana se puede pedir como un número (del 1 al 7) o como una
+cadena (de lunes a domingo). Se debe comprobar que el usuario introduce
+los datos correctamente y que el segundo día es posterior al primero
 .
 
  * @author Alberto Parra Toval
  */
 public class Ejercicio30 {
     public static void main(String[] args) {
-        System.out.println("[Numeros enteros comprendidos entre dos números]");
-        System.out.println("----------------------------------");
+        System.out.println("[Programa que enseña el tramo entre dos horas]");
+        System.out.println("----------------------------------------------");
+
+        System.out.println("Introduce la hora del primer dia: ");
+        int hora1 = Integer.parseInt(System.console().readLine());
+        System.out.println("Introduce la hora del segundo dia: ");
+        int hora2 = Integer.parseInt(System.console().readLine());  
+        int diaSemana;
+        int dia = 24;
+        
+        
         
     }
 }
