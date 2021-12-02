3491123f3676f8d4894879295509e40026fc8eb7
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-02 10:42:52 +0100

    Serpiente

--- Tema06/Ex20_06.java
+++ Tema06/Ex20_06.java
@@ -1,3 +1,4 @@
+
 /** Escribe un programa que simule la tirada de dos dados. El programa deberá
     continuar tirando los dados una y otra vez hasta que en alguna tirada los dos
     dados tengan el mismo valor
@@ -6,7 +7,30 @@
  */
 public class Ex20_06 {
     public static void main(String[] args) {
-  
+        System.out.println("Programa que muestra una serpiente");
+        System.out.print("Introduzca la longituda de tu serpiente: ");
+        int snakeSize = Integer.parseInt(System.console().readLine());
+        System.out.println("Aqui tiene su serpiente\n");
+        // PINTO LA CABEZA CON 12 ESPACIOS
+        System.out.println("            @");
     
+        // VARIABLES PARA EL CUERPO DE LA SERPIENTE
+        int movPosition;
+        int position = 13;
+        
+        while (snakeSize> 1) {
+        // LAS TRES POSIBLES POSICIONES DEL CUERPO LA SERPIENTE
+        position += (int)(Math.random() * 3) - 1;
+    
+        // PINTA LOS ESPACIOS DE LAS POSICIONES
+        for (movPosition = 1; movPosition < position; movPosition++) {
+            System.out.print(" ");
+        }
+
+        // PINTA EL CUERPO
+        System.out.println("#");
+
+        snakeSize--;
+        }
     }
 }
--- Tema06/Ex26_06.java
+++ Tema06/Ex26_06.java
@@ -1,12 +1,30 @@
-/** Escribe un programa que simule la tirada de dos dados. El programa deberá
-    continuar tirando los dados una y otra vez hasta que en alguna tirada los dos
-    dados tengan el mismo valor
- *
+/** Realiza un programa que pinte una tableta de turrón con un bocado realizado de
+forma aleatoria. El ancho y el alto de la tableta se pide por teclado. El bocado se
+da alrededor del turrón, obviamente no se puede dar un bocado por en medio
+de la tableta.
  * @author Alberto Parra Toval
  */
 public class Ex26_06 {
     public static void main(String[] args) {
-  
+        System.out.println("Tableta de chocolate con bocados");
+        System.out.println("Introduce la anchura de la tableta: ");
+        int anchuraIntroducida = Integer.parseInt(System.console().readLine());
+        System.out.println("Introduce la altura de la tableta: ");
+        int alturaIntroducida = Integer.parseInt(System.console().readLine());
+        int position = 0;
+        for (int i = 0; i < alturaIntroducida; i++) {
+            System.out.print("\n*");
+            position++;
+            
+            for (int j = 0; j < anchuraIntroducida; j++) {
+                System.out.print("*");
+            position++;    
+            }    
+            
+        }
+       
+        
+        
     
     }
 }
