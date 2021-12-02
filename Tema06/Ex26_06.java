06e2b16840c35b0801b3b0cfdc13fb56a2e01995
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-02 09:27:35 +0100

    Dado de Figuras de poker

---
+++ Tema06/Ex26_06.java
@@ -0,0 +1,12 @@
+/** Escribe un programa que simule la tirada de dos dados. El programa deberá
+    continuar tirando los dados una y otra vez hasta que en alguna tirada los dos
+    dados tengan el mismo valor
+ *
+ * @author Alberto Parra Toval
+ */
+public class Ex26_06 {
+    public static void main(String[] args) {
+  
+    
+    }
+}
