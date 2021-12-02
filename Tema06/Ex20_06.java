4f8696cfb4838a0e8ff5267d4ae3ab6f893aa524
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-02 10:43:44 +0100

    Serpiente

--- Tema06/Ex20_06.java
+++ Tema06/Ex20_06.java
@@ -7,30 +7,6 @@
  */
 public class Ex20_06 {
     public static void main(String[] args) {
-        System.out.println("Programa que muestra una serpiente");
-        System.out.print("Introduzca la longituda de tu serpiente: ");
-        int snakeSize = Integer.parseInt(System.console().readLine());
-        System.out.println("Aqui tiene su serpiente\n");
-        // PINTO LA CABEZA CON 12 ESPACIOS
-        System.out.println("            @");
-    
-        // VARIABLES PARA EL CUERPO DE LA SERPIENTE
-        int movPosition;
-        int position = 13;
         
-        while (snakeSize> 1) {
-        // LAS TRES POSIBLES POSICIONES DEL CUERPO LA SERPIENTE
-        position += (int)(Math.random() * 3) - 1;
-    
-        // PINTA LOS ESPACIOS DE LAS POSICIONES
-        for (movPosition = 1; movPosition < position; movPosition++) {
-            System.out.print(" ");
-        }
-
-        // PINTA EL CUERPO
-        System.out.println("#");
-
-        snakeSize--;
-        }
     }
 }
--- Tema06/Ex22_06.java
+++ Tema06/Ex22_06.java
@@ -5,6 +5,30 @@ Escribe un programa que genere de forma aleatoria la tirada de cinco dados.
  */
 public class Ex22_06 {
     public static void main(String[] args) {
+        System.out.println("Programa que muestra una serpiente");
+        System.out.print("Introduzca la longituda de tu serpiente: ");
+        int snakeSize = Integer.parseInt(System.console().readLine());
+        System.out.println("Aqui tiene su serpiente\n");
+        // PINTO LA CABEZA CON 12 ESPACIOS
+        System.out.println("            @");
+    
+        // VARIABLES PARA EL CUERPO DE LA SERPIENTE
+        int movPosition;
+        int position = 13;
         
+        while (snakeSize> 1) {
+        // LAS TRES POSIBLES POSICIONES DEL CUERPO LA SERPIENTE
+        position += (int)(Math.random() * 3) - 1;
+    
+        // PINTA LOS ESPACIOS DE LAS POSICIONES
+        for (movPosition = 1; movPosition < position; movPosition++) {
+            System.out.print(" ");
+        }
+
+        // PINTA EL CUERPO
+        System.out.println("#");
+
+        snakeSize--;
+        }
     }
 }
