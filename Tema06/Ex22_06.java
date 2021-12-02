a0b2b7dfb0acc1ba5716cb0516a0f89bce7cb624
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-02 09:28:30 +0100

    Dado figuras de poker

--- Tema06/Ex22_06.java
+++ Tema06/Ex22_06.java
@@ -5,33 +5,6 @@ Escribe un programa que genere de forma aleatoria la tirada de cinco dados.
  */
 public class Ex22_06 {
     public static void main(String[] args) {
-        System.out.println("Realiza una tirada de dados con figuras del poker");
-        String dados = "";
-        
-        for (int i = 0; i < 5; i++) {
-            switch((int)(Math.random() * 6)) {
-                case 0:
-                    dados = "As";
-                    break;
-                case 1:
-                    dados = "K";
-                    break;
-                case 2:
-                    dados = "Q";
-                    break;
-                case 3:
-                    dados = "J";
-                    break;
-                case 4:
-                    dados = "7";
-                    break;
-                case 5:
-                    dados = "8";
-                    break;
-                    default:
-                }
-                System.out.print(dados + " ");
-        }
         
     }
 }
