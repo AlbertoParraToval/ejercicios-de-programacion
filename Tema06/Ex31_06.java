ce63522447118d0c2bcf2cc1d591c4f19a41dda6
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-09 10:35:58 +0100

    CRABS

--- Tema06/Ex31_06.java
+++ Tema06/Ex31_06.java
@@ -1,44 +1,58 @@
-/**Realiza una version del programa Hola mundo.Se debe mostrar la palabra HOLA
- * (todo en mayúsculas) hecha con asteriscos.La altura de la palabra debe ser un
- * número impar generado al azar entre el 3 y el 13 ambos incluidos.Los palos ho
- * -rizontales de la H y de la A están justo a la mitad de la altura. La anchura
- * de las letras siempre es la misma. Aunque, en este caso , sólo hay 6 posibles
- * HOLAs de distinta altura,el programa debe hacerse de tal forma que sea muy fá
- * -cil adaptarlo a otros rangos de altura, por ej. entre 7 y 33. Por tanto, no 
- * se da por válido escribir 6 sentencias if, cada una pintando la correspondien
- * te figura completa.
+/**Realiza el juego del Craps. Las reglas son las siguientes: Al comenzar la
+partida, el jugador introduce la cantidad de dinero que quiere apostar. Se
+muestra la tirada aleatoria de dos dados. Si entre los dos dados suman 7 u
+11, el jugador gana la misma cantidad que apostó y termina la partida. Por ej.
+si apostó 1000 €, gana otros 1000 € y acaba con 2000 €. Si entre los dos dados
+suman 2, 3 o 12, el jugador pierde todo su dinero y termina la partida. Si no se
+da ninguno de los casos anteriores, es decir si sale 4, 5, 6, 8, 9 o 10, el juego
+entra en una segunda etapa. En esta etapa, el jugador buscará volver a obtener
+ese número en los dados. Si consigue repetir ese número, gana. Si sale un 7,
+pierde. Si sale otro número, tiene que seguir tirando.
  *
  * @author Alberto Parra Toval
  */
 public class Ex31_06 {
     public static void main(String[] args) {
-        System.out.println("El programa pinta el mensaje HOLA con una altura aleatoria.");
-        //NUMERO ALEATORIO ENTRE EL 3 Y 13
-        int altura=(int)(Math.random()* 13)+3;
-        //
-        if ((altura %2 )==0){
-        altura++;
-        }
-        // MIENTRAS I SEA MENOR O IGUAL QUE ALTURA
-        for (int i = 1; i <= altura; i++ ){
-            //SI I ES IGUAL A 1 SE PINTA LA PARTE DE ARRIBA
-        if (i == 1){
-            System.out.print("*    *   ****    *        **** ");
-            //SI I ES MENOS QUE ALTURA Y ESTA DIVISIBLE ENTRE 3 SE PINTA LA PARTE CONTINUA A LA DE ARRIBA
-        }else if (i < altura /2 +1){
-            System.out.print("*    *  *    *   *       *    * ");
-            //SI I IGUAL ALTURA Y ESTA DIVISIBLE ENTRE 3 SE PINTA LA PARTE DEL MEDIO
-        }else if (i == altura /2 +1){
-            System.out.print("******  *    *   *       ****** ");
-            //SI I MAYOUR QUE ALTURA ENTRE DOS Y I MENOS QUE ALTURA SE PINTA LA PARTE DE EN MEDIO PARA ABAJO
-        }else if ((i > altura /2)&&(i < altura)){
-            System.out.print("*    *  *    *   *       *    * ");
-              //SI I IGUAL A ALTURA SE PINTA LA PARTE DE ABAJO Y SE FINALIZA
-        }else if (i == altura ){
-            System.out.print("*    *   ****    ******  *    * ");
-        }
-            //ESPACIO SI NO SE CUMPLEN NINGUNA DE LAS SIGUIENTES CONDICIONES
+        System.out.println("[CRABS]");
+        System.out.println("------");
+        System.out.print("Introduce la cantidad de la puesta (Euros): ");
+        int apuesta = Integer.parseInt(System.console().readLine()) ;
+        int dado1 = (int)(Math.random()*6)+1;
+        int dado2 = (int)(Math.random()*6)+1;
+        System.out.println("El primer dado ha sido un: "+ dado1);
+        System.out.println("El segundo dado ha sido un: "+ dado2);
+        int sumaDados = dado1 + dado2;
+        System.out.println("La suma de los dado ha sido : " + sumaDados);
+    
+        int premio = apuesta;
+        if ((sumaDados == 7)||(sumaDados==11)){
+        premio = premio + apuesta;
+        System.out.println("Has ganado.");
+        System.out.println("Has acabado con:"+premio+"€");
+        }else if (((sumaDados == 2)||(sumaDados == 3))||(sumaDados == 12)){
+        System.out.println("Has perdido.");
+        }else{
         System.out.println("");
+        System.out.println("Has entrado en la segunda etapa. ");
+        System.out.println("---------------------------------");
+        do{
+        
+        dado1 = (int)(Math.random()*6)+1;
+        dado2 = (int)(Math.random()*6)+1;
+        System.out.println("El primer dado ha sido un: " + dado1);
+        System.out.println("El segundo dado ha sido un: " + dado2);
+        if ((dado1 + dado2) == sumaDados){
+            premio = premio + apuesta;
+            System.out.println("Has ganado.");
+            System.out.println("Has acabado con:" + premio + "€");
+        }else if ((dado1 + dado2) == 7){
+            System.out.println("Has perdido.");
+        }else{
+            System.out.println("La suma de los dados ha sido "+ (dado1 + dado2));
+            System.out.print("Vuelve a tirar (Press any key):");
+            String tirada = (System.console().readLine()) ;
+        }
+            }while (((dado1 + dado2) != 7) && (dado1 + dado2) != sumaDados);
         }
     }
 }
