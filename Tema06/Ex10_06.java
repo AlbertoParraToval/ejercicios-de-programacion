46ee37854004c00f535693435a9448bc10149122
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-11 18:46:59 +0100

    10 Lineas de Caracteres aleatorios

--- Tema06/Ex10_06.java
+++ Tema06/Ex10_06.java
@@ -1,5 +1,3 @@
-import javax.sound.sampled.LineEvent;
-
 /** Realiza un programa que pinte por pantalla diez líneas formadas por caracteres.
 El carácter con el que se pinta cada línea se elige de forma aleatoria entre uno
 de los siguientes: *, -, =, ., |, @. Las líneas deben tener una longitud aleatoria
@@ -11,29 +9,52 @@ entre 1 y 40 caracteres.
 public class Ex10_06 {
     public static void main(String[] args) {
         System.out.println("[Programa que pinta diez lineas formadas por caracteres]");
-        int longitud = 10;
-
-        for (int i = longitud; i <= 40; i++) {
-            i++;
-            int caracter = (int)(Math.random()*5); 
-            switch (caracter) {
-                case 1:
-                System.out.print(" @ ");
-                    break;
-                case 2:
-                System.out.print(" * ");
-                    break;
-                case 3:
-                System.out.print(" = ");
-                    break;
-                case 4:
-                System.out.print(".");
-                    break;
-                default:
-                System.out.print(" | ");
-                    break;
+        int numAleatorio=0;
+        int longitudLinea=0;
+        int contador = 0;
+        while (contador < 10){
+        
+        numAleatorio=(int)(Math.random()*6);
+        switch (numAleatorio){
+        case 0:
+            longitudLinea=(int)(Math.random()*40)+1;
+            for (int i = 0; i < longitudLinea; i++){
+            System.out.print("*");
             }
+        break;
+        case 1:
+            longitudLinea=(int)(Math.random()*40)+1;
+            for (int i = 0; i < longitudLinea; i++){
+            System.out.print("-");
+            }
+        break;
+        case 2:
+            longitudLinea=(int)(Math.random()*40)+1;
+            for (int i = 0; i < longitudLinea; i++){
+            System.out.print("=");
+            }
+        break;
+        case 3:
+            longitudLinea=(int)(Math.random()*40)+1;
+            for (int i = 0; i < longitudLinea; i++){
+            System.out.print(".");
+            }
+        break;
+        case 4:
+            longitudLinea=(int)(Math.random()*40)+1;
+            for (int i = 0; i < longitudLinea; i++){
+            System.out.print("|");
+            }
+        break;
+        default:
+            longitudLinea=(int)(Math.random()*40)+1;
+            for (int i = 0; i < longitudLinea; i++){
+            System.out.print("@");
+            }
+        break;
         }
-
+        System.out.println("");
+        contador++;
+        }
     }
 }
