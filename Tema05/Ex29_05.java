4084917c2ccd7a67b4bf71970041c5d5d6b7305d
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-11 17:53:20 +0100

    A partir de un Num, sus predecesores y si son divisibles entre este

--- Tema05/Ex29_05.java
+++ Tema05/Ex29_05.java
@@ -1,31 +1,31 @@
+/**
+ * Escribe un programa que muestre por pantalla todos los números enteros positi
+ * -vos menores a uno leído por teclado que no sean divisibles entre otro también
+ * leído de igual forma.
+ * 
+ * @author Alberto Parra Toval
+ * 
+ * 
+ */
 public class Ex29_05 {
     public static void main(String[] args) {
-        System.out.println("[Piramide]");
-        System.out.println("--------------");
-        System.out.print("Introduzca la altura de la pirámide: ");
-        int alturaIntroducida = Integer.parseInt(System.console().readLine());
-    
-        System.out.print("Introduzca el carácter de relleno: ");
-        int caracter = Integer.parseInt(System.console().readLine());;
-        
-        int base = 1;
-        int longitud = 1;
-        int espacios = alturaIntroducida-1;
-        
-        while (base <= alturaIntroducida) {
-        
-          for (int i = 1; i <= espacios; i++) {
-            System.out.print(" ");
+      System.out.println("A partir de un numero muestra los menores a ese num y que no son divisibles por el num elegido");
+      //Pedimos al usuario un divisor y un numero
+      System.out.print("Introduce un numero por teclado: ");
+      int num= Integer.parseInt(System.console().readLine()) ;
+      System.out.print("Introduce un divisor por teclado: ");
+      int div = Integer.parseInt(System.console().readLine()) ;
+      //Variable para comrpobar si es divisible
+      int comprobador = 0;
+      // Mientras el num introducido sea mayor a 0
+      while (num > 0){
+        num--;
+        comprobador = num % div;
+        //Si comprobado no es igual a 0 mostramos esos numeros ya que no son divisibles por el introducido
+        if (comprobador != 0){
+          System.out.print(num +" ");
           }
-    
-          for (int i = 1; i <= longitud; i++) {
-            System.out.print(caracter);
-          }
-    
-          System.out.println();
-          base++;
-          espacios--;
-          longitud += 2;
-        }
+      }
+      System.out.println(" ");
     }
-}
+  }
