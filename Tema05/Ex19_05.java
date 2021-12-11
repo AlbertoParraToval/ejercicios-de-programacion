d553aa9b8ba41ea15e42a2354e1ee6298a8a0fcc
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-11 17:14:13 +0100

    Piramide 1.0

--- Tema05/Ex19_05.java
+++ Tema05/Ex19_05.java
@@ -4,34 +4,34 @@
     pedir por teclado.
  * @author Alberto Parra Toval
  */
-public class Ex11_05 {
+public class Ex19_05 {
     public static void main(String[] args) {
         System.out.println("[Piramide]");
         System.out.println("--------------");
-        System.out.print("Introduzca la altura de la pirámide: ");
-        int alturaIntroducida = Integer.parseInt(System.console().readLine());
-    
-        System.out.print("Introduzca el carácter de relleno: ");
+        //Pedimos al usuario que nos diga la altura y el caracter ha usar
+        System.out.print("Introduce la altura de la piramide por teclado: ");
+        int altura = Integer.parseInt(System.console().readLine()) ;
+        System.out.print("Introduce el caracter por teclado: ");
         String caracter = System.console().readLine();
-        
+        //Definimos la base, numespacio y la longitudlinea
         int base = 1;
-        int longitud = 1;
-        int espacios = alturaIntroducida-1;
-        
-        while (base <= alturaIntroducida) {
-        
-          for (int i = 1; i <= espacios; i++) {
+        int numespacio = altura -1;
+        int longitudLinea = 1;
+        // El bucle se ejecutara hasta que la base sea mayor a la altura introducida
+        while (base <= altura ){
+          // Mientras a sea menor o igual al nuemro de espacio no se pinta nada
+          for (int a = 1; a <= numespacio; a++){
             System.out.print(" ");
           }
-    
-          for (int i = 1; i <= longitud; i++) {
+          //Mientras a es menor o igual a longitud linea que pinte el caracter introducido
+          for (int a = 1; a <= longitudLinea; a++){
             System.out.print(caracter);
           }
-    
-          System.out.println();
+          System.out.println(" ");
+          //Cada vez que se ejecute el buble se sume una base, se reste un espacio y se sume dos a a longitudLiena
           base++;
-          espacios--;
-          longitud += 2;
+          numespacio--;
+          longitudLinea += 2;
         }
+      }
     }
-}
