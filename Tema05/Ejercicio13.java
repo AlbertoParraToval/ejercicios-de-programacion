50ced4211b384a32e0fba011e10806b239789e51
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-11 12:33:25 +0100

    Contador de Numeros Negativos y Positivos

--- Tema05/Ejercicio13.java
+++ Tema05/Ejercicio13.java
@@ -1,21 +1,20 @@
-
-
 /**
- *  Muestra la tabla de multiplicar de un número introducido por teclado.
+ *  Programa que cuenta cuantos numeros positivos y negativos has introducido
  *
  * @author Alberto Parra Toval
  */
 public class Ejercicio13 {
     public static void main(String[] args) {
       System.out.println("Programa que lee diez numeros y determine los negativos/positivos");
-      System.out.println("Introduce diez números: ");
-      int negativos = 0;
-      int positivos = 0;
-
+      System.out.println("Introduce diez numeros: ");
+      int negativos = 0; //Contador de numeros negativos
+      int positivos = 0; //Contador de numeros positivos
+      //Definimos el numero maximo hasta 10 veces
       for (int numeroMax = 0; numeroMax < 10; numeroMax++) {
+        //Le ponemos la condicion que si el numero introducido es mayor que 0 que sume al contador de los positivos
         if ( Integer.parseInt(System.console().readLine()) > 0) {
           positivos++;
-        } else {
+        } else {   // Sino pues que sume al contador de los negativos
           negativos++;
         }
       }
