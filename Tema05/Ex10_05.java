9bf0384c16b46e850c9d8071343daf474d1125fa
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-11 16:46:43 +0100

    Media de numeros positivos

--- Tema05/Ex10_05.java
+++ Tema05/Ex10_05.java
@@ -8,17 +8,20 @@
  */
 public class Ex10_05 {
     public static void main(String[] args) {
+        // Defino tanto mi contador como la suma y pido la introduccion de un numero
         System.out.println("Introduce los numeros que desees: ");
         int num = Integer.parseInt(System.console().readLine());  
-        int numeros = 0;
+        int contador = 0;
         int suma = 0;
+        // El programa seguira pidiendo numeros y sumando al contador mientras no introduzcamos un numero negativo
         while(num >= 0){
             System.out.println("Introduce el siguiente numero: ");
             num = Integer.parseInt(System.console().readLine());  
-            numeros++;
+            contador++;
             suma += num;
         }  
-        System.out.println("La media de los numeros elegidos es de:" + (suma - num)/(numeros - 1));
+        // Muestro la media de los numeros elegidos
+        System.out.println("La media de los numeros elegidos es de:" + (suma - num)/(contador - 1));
         
     }
   }
