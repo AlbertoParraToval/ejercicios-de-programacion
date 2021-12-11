033e3a4e15051b38a54a247268ed9a60a716f276
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-11 16:39:39 +0100

    Tabla de Multiplicar

--- Tema05/Ex08_05.java
+++ Tema05/Ex08_05.java
@@ -6,12 +6,14 @@
 public class Ex08_05 {
     public static void main(String[] args) {
       System.out.println("Ejercicios de Tablas");
-      System.out.println("Introduce el número que desees: ");
+      // Introducimos el numero que queramos
+      System.out.println("Introduce el numero que desees: ");
       int num = Integer.parseInt(System.console().readLine());
       System.out.println("[La tabla del " + num + "]");
       System.out.println("---------");
-
+      // Definimos que se repita dicha multiplicacion hasta 10 veces definiendo mult < 11
       for (int mult = 1; mult < 11 ; mult++){    
+        //Mostramos los resultados por pantalla
       System.out.println(num + "x" + mult + " = " + num * mult);
       System.out.println("---------");
       }
