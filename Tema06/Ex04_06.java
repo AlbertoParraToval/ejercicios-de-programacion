df20bd42c24d40e0e65dbdddefc73bb13402f514
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-11 18:29:41 +0100

    20 Num aleatorios del 0 al 10

--- Tema06/Ex04_06.java
+++ Tema06/Ex04_06.java
@@ -5,12 +5,10 @@
 public class  Ex04_06 {
     public static void main(String[] args) {
         System.out.println("Programa que muestra 20 numeros aleatorios de o a 10 ");
-        
+        // Mientras i sea igual o menor a 20 que se generen numeros aleatorios entre el 0 y el 10
         for (int i = 0; i <= 20; i++) {
           System.out.print(((int)(Math.random()*10)) + " ");
           }
       System.out.println( );
-
-      
     }
 }
