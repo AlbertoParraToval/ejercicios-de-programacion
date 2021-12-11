9df6c1dbc5accf31747b373774237cfdb8c52d6e
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-11 18:31:54 +0100

    50 Numeros entre 100 y 199, Max,Min,Media

--- Tema06/Ex05_06.java
+++ Tema06/Ex05_06.java
@@ -7,24 +7,27 @@ de esos números.
 public class  Ex05_06 {
     public static void main(String[] args) {
         System.out.println("Programa que muestra 50 numeros aleatorios y dice el mayor , el menos y su media");
+        //Definimos las variables 
         int maximo = 100;
         int minimo = 199;
         int suma = 0;
         int n;
-          
+        // Mientras i sea menor que 50 que se generen numeros aleatorios entre 100 y 199
         for (int i = 0; i < 50; i++) {
           n = (int)(Math.random()*100) + 100;
           System.out.print(n + " ");
+          //Se vayan sumando los numeros que se generen
           suma += n;
-            
+          // obtenemos el minimo
           if (n < minimo) {
             minimo = n;
           }
-            
+          //Obtenemos el maxima
           if (n > maximo) {
             maximo = n;
           }
         }
+        //Mostramos los resultados 
         System.out.println( );
         System.out.println("Num.Min: " + minimo);
         System.out.println("Num.Max: " + maximo);
