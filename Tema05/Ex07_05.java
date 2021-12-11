166304257ba8016df1ea052f28bbbbbf32a16600
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-11 16:35:42 +0100

    Caja Fuerte 1.0

--- Tema05/Ex07_05.java
+++ Tema05/Ex07_05.java
@@ -11,20 +11,23 @@ public class Ex07_05{
     public static void main(String[] args) {
       System.out.println("Ejercicio Caja Fuerte");
       System.out.println("Para abrir la caja fuerte dispone de 4 intentos, suerte.");
+      // Introducimos la contraseña en la consola que creemos que es
       System.out.println("Introduce la contraseña: ");
       int contraseña = Integer.parseInt(System.console().readLine());      
+      // Definimos el codigo oculto y el numero de intentos
       int codigo = 1234;
       int intentos = 1;
       do {
+        // Si contraseña = codigo
           if (contraseña == codigo) {
               System.out.println("La caja fuerte se ha abierto satisfactoriamente");
               intentos = 7;
-          } else {
+          } else { //Si contraseña != codigo
             intentos++;  
             System.out.println("Lo siento, esa no es la combinación");
             System.out.println("Introduce la contraseña de nuevo, por favor: ");
             contraseña = Integer.parseInt(System.console().readLine()); 
           }
-        }  while (intentos < 4);
+        }  while (intentos < 4); // Mientras el numero de intentos sea menos a 4 que se ejecute el bucle
     }
 }
