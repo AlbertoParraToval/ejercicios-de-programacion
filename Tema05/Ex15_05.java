a3d9153478743e6ce0009c9a7d51eb947033d1d6
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-11 17:02:05 +0100

    Numero, base y exponente 2.0

--- Tema05/Ex15_05.java
+++ Tema05/Ex15_05.java
@@ -8,18 +8,21 @@
 
  * @author Alberto Parra Toval
  */
-public class Ex11_05 {
+public class Ex15_05 {
     public static void main(String[] args) {
         System.out.println("[Ejercicio Base y Exponente]");
         System.out.println("----------------------------");
+        // Pedimos al usuario que introduzca tanto la base como el exponente
         System.out.print("Introduzca la base: ");
         int base = Integer.parseInt(System.console().readLine());
         System.out.print("Introduzca el exponente: ");
         int exponenteIntroducido = Integer.parseInt(System.console().readLine());
-    
-       for (int i = 1; i <= exponenteIntroducido; i++) {
-        System.out.println("El resultado es: " + base + "^" + i);
-        System.out.println("--------------------");
-       }
+        
+        // Definimos el buble hasta que nuestra variable auxiliar i supere a nuestro exponenete introducido
+        for (int i = 1; i <= exponenteIntroducido; i++) {
+        // Mostramos el resultado por pantalla
+            System.out.println("El resultado es: " + base + "^" + i);
+            System.out.println("--------------------");
+        }
     }
 }
