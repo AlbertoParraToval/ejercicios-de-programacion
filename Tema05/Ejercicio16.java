0e68e78d44adb13944cff4344815ea63b74773c3
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-11 12:36:25 +0100

    Numero Introducido Primo o NoPrimo

--- Tema05/Ejercicio16.java
+++ Tema05/Ejercicio16.java
@@ -8,20 +8,22 @@ public class Ejercicio16 {
     public static void main(String[] args) {
         System.out.println("[Numeros Primos o no Primos]");
         System.out.println("----------------------------");
-        System.out.print("Introduzca un número entero: ");
+        System.out.print("Introduzca un numero entero: ");
         int numeroIntroducido = Integer.parseInt(System.console().readLine());
         boolean numeroPrimo = true;
         
+        // Definimos que i=2 para ahora dividirlo entre nuestro numero introducido y que si el resultado es == 0, numeroPrimo = false
         for (int i = 2; i < numeroIntroducido; i++) {
           if ((numeroIntroducido % i) == 0) {
             numeroPrimo = false;
           }
         }
-            
+        
+        //Mostramos el resultado de la operacion anterior
         if (numeroPrimo) {
-          System.out.println("El número introducido es primo.");
+          System.out.println("El numero introducido es primo."); // El numero introducido al dividirlo por i no ha dado 0
         } else {
-          System.out.println("El número introducido no es primo.");
+          System.out.println("El numero introducido no es primo.");// El numero introducido al dividirlo por i ha dado 0
         }
       }
     }
