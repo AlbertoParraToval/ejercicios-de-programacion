57599b4036c986cc89a600f573295be331f0750f
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-11 17:45:10 +0100

    Cuenta, Suma Multiplos de 3 entre 1 y X numero

--- Tema05/Ex27_05.java
+++ Tema05/Ex27_05.java
@@ -5,23 +5,21 @@
  */
 public class Ex27_05 {
   public static void main(String[] args) {
-    System.out.println("Ejercicio que muestra las suma de 100 numeros a partir de uno");
-    int numeroIntroducido = 0;
-
-    do {
-        System.out.print("Introduzca un numero entero positivo: ");
-        numeroIntroducido = Integer.parseInt(System.console().readLine());
-    
-        if(numeroIntroducido < 1) {
-      System.out.println("El número introducido no es correcto.");
-        }
-    } while (numeroIntroducido < 1);
-
-    int multiplo = 3;
-    for(int n = numeroIntroducido; n < numeroIntroducido ; n++) {
-      n += multiplo;
-      System.out.println(numeroIntroducido + " ");
+    System.out.println("El programa muestra, cuenta y suma los multiplos de 3 corprendidos entre 1 y el numero elegido por el usuario.");
+    // Pedimos al usuario un numero
+    System.out.print("Introduce un numero por teclado: ");
+    int numero = Integer.parseInt(System.console().readLine()) ;
+    //Definimos las variables
+    int multiplo = 0;
+    int sumarMultiplo=0;
+    // Mientras multiplo sea menor que el numero que introduzcamos
+    while (multiplo < numero){
+      multiplo+=3;
+      System.out.print(multiplo +" ");
+      sumarMultiplo = sumarMultiplo + multiplo;
     }
+    //Mostramos los resultados
     System.out.println(" ");
+    System.out.print("La suma de los multiplos es: "+ sumarMultiplo +" .");
   }
 }
