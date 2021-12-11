33654b53aaa2dead3e3de2388e2200de17f43610
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-11 17:27:39 +0100

    Piramide de Numeros

--- Tema05/Ex24_05.java
+++ Tema05/Ex24_05.java
@@ -1,32 +1,44 @@
+/**
+ * Escribe un programa que lea un número n e imprima una pirámide de números con
+ * n filas como en la siguiente figura:
+ * 
+ * @author Alberto Parra Toval
+ * 
+ * 
+ */
 public class Ex24_05 {
     public static void main(String[] args) {
-        System.out.println("[Piramide]");
-        System.out.println("--------------");
-        System.out.print("Introduzca la altura de la piramide: ");
-        int alturaIntroducida = Integer.parseInt(System.console().readLine());
-        
-        int altura = 1;
-        int i = 0;
-        int espacios = alturaIntroducida-1;
-        
-        while (altura <= alturaIntroducida) {
-        
-          for (i = 1; i <= espacios; i++) {
-            System.out.print(" ");
-          }
-    
-          for (i = 1; i <= altura; i++) {
-            System.out.print(i);
-          }
+        System.out.println("[Piramide de Numeros]");
+        System.out.println("---------------------");
+        // Pedimos la altura al usuario y definimos nuestras varibles
+        System.out.print("Introduce un numero por teclado: ");
+        int altura = Integer.parseInt(System.console().readLine()) ;
+        int linea = 1;
+        int n = 0;
+        int espaciado = altura - 1;
 
-          for (i = altura; i > 0; i--) {
-            System.out.print(i);
-          }
-
-          System.out.println();
-          altura++;
-          espacios--;
-        
-        }
+        // Mientras linea sea menor o igual altura
+          while (linea <= altura){
+            //Espaciados
+            for (int espacio = 1; espacio <= espaciado; espacio++){
+              System.out.print(" ");
+              }
+            //Pintando linea
+            for (int largoLinea = 1; largoLinea <= n; largoLinea++){
+              System.out.print(largoLinea);
+              }
+            //Pintando Filas
+            for (int filas = linea; filas > 0; filas--){
+              System.out.print(filas);
+            }
+            System.out.println(" ");
+            
+            //aumenta el numero de filas
+            linea++;
+            //aumenta el numero de la cifra de caracteres
+            n++;
+            // dismimnuye la cantidad de espacios
+            espaciado--;
+            }
+      }
     }
-}
