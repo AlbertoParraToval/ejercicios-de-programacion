43582e76864a5c77151c2a73690bc1c9b84170e7
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-11 17:06:50 +0100

    A partir de un numero sumar los 100 siguientes

--- Tema05/Ex17_05.java
+++ Tema05/Ex17_05.java
@@ -4,23 +4,26 @@
     es correcto (que es un número positivo).
 * @author Alberto Parra Toval
 */
-public class Ex11_05 {
+public class Ex17_05 {
     public static void main(String[] args) {
         System.out.println("Ejercicio que muestra las suma de 100 numeros a partir de uno");
+        //Defino la variable num
         int numeroIntroducido = 0;
-        do {
-            System.out.print("Introduzca un número entero positivo: ");
-            numeroIntroducido = Integer.parseInt(System.console().readLine());
         
+        do {
+            System.out.print("Introduzca un numero entero positivo: ");
+            numeroIntroducido = Integer.parseInt(System.console().readLine());
+          // Si el nuemro introducido es menor a 0:
             if(numeroIntroducido < 0) {
-          System.out.println("El número introducido no es correcto.");
+          System.out.println("El numero introducido no es correcto.");
             }
-        } while (numeroIntroducido < 0);
+        } while (numeroIntroducido < 0); //Mientra el numero introducido sea menor a 0 que se ejecute el buble
 
         int suma = 0;
+        // Mientras i sea menor al numero introducido mas 100 que el buble se ejecute
         for(int i = numeroIntroducido; i < numeroIntroducido + 100; i++) {
           suma += i;
         }
-        System.out.println("La suma de los siguientes 100 números al " + numeroIntroducido + " es " + suma);
+        System.out.println("La suma de los siguientes 100 numeros al " + numeroIntroducido + " es " + suma);
     }
 }
