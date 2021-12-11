625693efd0337db93861104eeaff9f7803632cca
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-11 18:34:43 +0100

    Num 0 a 100, Adivinarlo

--- Tema06/Ex06_06.java
+++ Tema06/Ex06_06.java
@@ -6,34 +6,35 @@ menor o mayor que el número secretos
  */
 public class  Ex06_06 {
     public static void main(String[] args) {
+      //Definimos las variables
       int oportunidades = 5;
       int numeroIntroducido;
       boolean acertado = false;
       int numeroMisterioso = (int)(Math.random() * 101);
       
       System.out.println("Estoy pensando un numero del 0 al 100, intenta adivinarlo. Tienes 5 oportunidades.");
-  
+      
       do {
         System.out.print("Introduce un numero: ");
         numeroIntroducido = Integer.parseInt(System.console().readLine());
         oportunidades--;
-  
+        // Si es menor
         if ( (numeroIntroducido > numeroMisterioso) && (oportunidades > 0) ){
           System.out.println("El numero que estoy pensando es menor que " + numeroIntroducido);
           System.out.println("Te quedan " + oportunidades + " oportunidades.");
         }
-        
+        // Si es mayor
         if ( (numeroIntroducido < numeroMisterioso) && (oportunidades > 0) ){
           System.out.println("El numero que estoy pensando es mayor que " + numeroIntroducido);
           System.out.println("Te quedan " + oportunidades + " oportunidades.");
         }
-        
+        // Si has acertado
         if (numeroIntroducido == numeroMisterioso) {
           acertado = true;
           System.out.println("¡Enhorabuena! ¡has acertado!");
         }
-      } while (!acertado && (oportunidades > 0));
-      
+      } while (!acertado && (oportunidades > 0)); // Mientras no acierte el numero y oportunidades sea mayor a 0
+      // Si no has acertado y te has quedado sin intentos
       if (!acertado) {
         System.out.println("Lo siento, no has acertado, el numero que estaba pensando era el " + numeroMisterioso);
       }
