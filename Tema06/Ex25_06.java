5e32a9365c936adede9d72919706a8017e6ebe2c
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-12 18:34:54 +0100

    50 NUM (-100/200) Primo NoPrimo

--- Tema06/Ex25_06.java
+++ Tema06/Ex25_06.java
@@ -8,23 +8,25 @@ almohadillas (p. ej. #19#) y los múltiplos de 5 entre corchetes (p. ej. [25]).
 public class Ex25_06 {
     public static void main(String[] args) {
         System.out.println("[Programa que muestra 50 numeros entre el -100 y el 200 , su max par y la media de todos]");
+        // Que se generen 100 numeros
         for (int i = 0; i < 100; i++) {
             int numero = (int)(Math.random() * 191) + 10;
-            boolean esPrimo = true;
-            for (int j = 2; j < numero; j++) {
+            boolean esPrimo = true; // Definimos esPrimo como true d
+            for (int j = 2; j < numero; j++) { // Si dividimos numero entre j que es igual a 2 y su resultado da 0, esPrimo es igual a false 
                 if ((numero % j) == 0) {
                 esPrimo = false;
                 }
             }
             
+            // Si esPrimo pues que muestre el numero entre almohadillas
             if (esPrimo) {
                 System.out.print(" #" + numero + "# ");
-            } else if ((numero % 5) == 0) {// múltiplo de 5
+            } else if ((numero % 5) == 0) {   // Si es divisible entre 5 que se muestre el numero en corchetes
                 System.out.print(" [" + numero + "] ");
             } else {
-                System.out.print(" " + numero + " ");
+                System.out.print(" " + numero + " "); // Que se muestren los demas numeros
             }
-          } // for
+        }
     }
 }
     
