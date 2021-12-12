c6503e2abcc4d65fc026603f76c803d8b55237c3
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-12 18:51:27 +0100

    CRABS

--- Tema06/Ex31_06.java
+++ Tema06/Ex31_06.java
@@ -15,44 +15,44 @@ public class Ex31_06 {
     public static void main(String[] args) {
         System.out.println("[CRABS]");
         System.out.println("------");
+        //El usuario introduce cuanto decide apostar
         System.out.print("Introduce la cantidad de la puesta (Euros): ");
         int apuesta = Integer.parseInt(System.console().readLine()) ;
-        int dado1 = (int)(Math.random()*6)+1;
-        int dado2 = (int)(Math.random()*6)+1;
-        System.out.println("El primer dado ha sido un: "+ dado1);
-        System.out.println("El segundo dado ha sido un: "+ dado2);
-        int sumaDados = dado1 + dado2;
+        int dado1 = (int)(Math.random()*6)+1; // Tirada del primer dado
+        int dado2 = (int)(Math.random()*6)+1; // Tirada del segundo dado
+        System.out.println("El primer dado ha sido un: "+ dado1); // Mostramos el resultado del 1
+        System.out.println("El segundo dado ha sido un: "+ dado2); // Mostramos el resultado del 1
+        int sumaDados = dado1 + dado2; // Los sumamos
         System.out.println("La suma de los dado ha sido : " + sumaDados);
     
-        int premio = apuesta;
-        if ((sumaDados == 7)||(sumaDados==11)){
-        premio = premio + apuesta;
+        int premio = apuesta; 
+        if ((sumaDados == 7)||(sumaDados==11)){ // Si se da una de estas combinaciones
+        premio = premio + apuesta; // El premio es igual al premio de antes mas la apuesta
         System.out.println("Has ganado.");
         System.out.println("Has acabado con:"+premio+"€");
-        }else if (((sumaDados == 2)||(sumaDados == 3))||(sumaDados == 12)){
-        System.out.println("Has perdido.");
+        }else if (((sumaDados == 2)||(sumaDados == 3))||(sumaDados == 12)){ // Si se da una de estas combinaciones
+        System.out.println("Has perdido."); // has perdido
         }else{
         System.out.println("");
-        System.out.println("Has entrado en la segunda etapa. ");
+        System.out.println("Has entrado en la segunda etapa. "); // Si no ha pasado ninguna de las siguiente combinaciones pasas a la 2 ronda 
         System.out.println("---------------------------------");
         do{
-        
-        dado1 = (int)(Math.random()*6)+1;
-        dado2 = (int)(Math.random()*6)+1;
+        dado1 = (int)(Math.random()*6)+1;  // Tirada del primer dado
+        dado2 = (int)(Math.random()*6)+1; // Tirada del segundo dado
         System.out.println("El primer dado ha sido un: " + dado1);
         System.out.println("El segundo dado ha sido un: " + dado2);
-        if ((dado1 + dado2) == sumaDados){
-            premio = premio + apuesta;
+        if ((dado1 + dado2) == sumaDados){  // Si se da esta opcion
+            premio = premio + apuesta; // El premio es igual al premio de antes mas la apuesta
             System.out.println("Has ganado.");
             System.out.println("Has acabado con:" + premio + "€");
-        }else if ((dado1 + dado2) == 7){
+        }else if ((dado1 + dado2) == 7){ // Si se ada esta opcion 
             System.out.println("Has perdido.");
         }else{
-            System.out.println("La suma de los dados ha sido "+ (dado1 + dado2));
+            System.out.println("La suma de los dados ha sido "+ (dado1 + dado2)); // Si no se da otra vez ninguna de las combinaciones
             System.out.print("Vuelve a tirar (Press any key):");
             String tirada = (System.console().readLine()) ;
         }
-            }while (((dado1 + dado2) != 7) && (dado1 + dado2) != sumaDados);
+            }while (((dado1 + dado2) != 7) && (dado1 + dado2) != sumaDados); // Si ninguna de las opciones se dan pues que se repita el bucle
         }
     }
 }
