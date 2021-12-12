cb663b71d329e8852b69693d5ecf91502c9755de
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-12 11:46:31 +0100

    20 Num con Nota

--- Tema06/Ex11_06.java
+++ Tema06/Ex11_06.java
@@ -5,42 +5,47 @@
 public class Ex11_06 {
     public static void main(String[] args) {
         System.out.println("[Programa que muestra 20 notas generadas al azar]");
+        //Defino las varibles 
         int notas;
         int numBien = 0;
         int numNotable = 0;
         int numSobre = 0;
         int numSuficientes = 0;
         int numSuspensos = 0;
+        
+        //El bucle se repetira 20 veces  
         for (int i = 1; i <= 20; i++) {
-            notas = (int)(Math.random()*9 + 1);
+            notas = (int)(Math.random()*9 + 1);  // La variable notas se genera un numero aleatorio del 0 al 10
             switch (notas) {
-                case 1:
+                case 1: //Si la nota es sobresaliente
                 System.out.println("Sobresaliente");
                 numSobre++;
                     break;
 
-                case 2:
+                case 2: //Si la nota es notable
                 System.out.println("Notable");
                 numNotable++;
                     break;
 
-                case 3:
+                case 3: //Si la nota es Bien
                 System.out.println("Bien");
                 numBien++;
                     break;
             
-                case 4:
+                case 4: // Si la nota es suficiente
                 System.out.println("Suficiente");
                 numSuficientes++;
                     break;
             
-                default:
+                default: //Si la nota es suspenso
                 System.out.println("Suspenso");
                 numSuspensos++;
                     break;
             }
         }
-        System.out.println(" ");;
+
+        //Mostramos los resultados de los numeros que han salido
+        System.out.println(" ");
         System.out.println("Las notas con sobresaliente son: " + numSobre);
         System.out.println("Las notas con notable son: " + numNotable);
         System.out.println("Las notas con bien son: " + numBien);
