7ad89f26173c9c7be1593737fc260e076cd782aa
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-12 12:01:37 +0100

    Programa Adivina Num

--- Tema06/Ex14_06.java
+++ Tema06/Ex14_06.java
@@ -7,34 +7,50 @@ te acaba de decir.
  */
 public class Ex14_06 {
     public static void main(String[] args) {
-        int oportunidades = 5;
-        int numeroIntroducido = 45;
-        boolean acertado = false;
-        int numeroMisterioso = (int)(Math.random() * 100);
+        System.out.println("El programa adivina un número que introduce el usuario entre 0 y 100.");
+        //Defino las variables
+        int contador = 5;
+        int maximo = 0;
+        int minimo = 199;
+        int numAleatorio=0;
+        int media = 0;
+        //Le pdio al usuario que intriduzca un numero que sera el numero que el programa tendra que adivinar
+        System.out.print("Introduce un numero entero para que el programa lo adivine: ");
+        int numIntroducido = Integer.parseInt(System.console().readLine());
         
-        System.out.println("Programa que intenta adivinar un numero del 0 al 100");
-    
-        do {
-            numeroMisterioso = (int)(Math.random() * 100);
-            if ( (numeroMisterioso > numeroIntroducido) && (oportunidades > 0) ){
-            System.out.println("El numero que estoy pensando es menor que " + numeroMisterioso);
-            System.out.println("Te quedan " + oportunidades + " oportunidades.");
+        //El programa creara un numero aleatorio
+        numAleatorio=(int)(Math.random()*100);
+        // Mientras sea menor o igual al contador se repite el siguiente bucle
+        for (int i = 1; i <= contador;i++){
+            //Si el programa adivina el numero
+            if (numAleatorio == numIntroducido){
+            System.out.println("Has adivinado el numero enorabuena!!!");
+            System.out.print("El numero era: "+numIntroducido);
+            System.out.println("");
+            i+=5;
+            }else if(i < contador){ // Si el progrmaa no adivina el numero
+            System.out.println("El numero del programa es: "+numAleatorio);
+            System.out.println("Sigue adivinando el numero.");
+            System.out.println("Le quedan "+(contador - i)+" intentos.");
+            if (numIntroducido < numAleatorio){ // Le deimos que el numero que ha puesto es mayor al oculto
+            System.out.println("El numero introducido es menor");
+            System.out.println("");
+            numAleatorio=(int)(Math.random()*numIntroducido);
+            }else{  // Le decimos que el numero que ha puesto es menor al oculto
+            System.out.println("El numero introducido es mayor");
+            System.out.println("");
+            int numAnterior=numAleatorio;
+                if (i > 1){
+                numAleatorio=(int)(Math.random()*(numAnterior - numIntroducido)) + numIntroducido;
+                }
             }
-        
-            if ( (numeroMisterioso < numeroIntroducido) && (oportunidades > 0) ){
-            System.out.println("El numero que estoy pensando es mayor que " + numeroMisterioso);
-            System.out.println("Te quedan " + oportunidades + " oportunidades.");
+        }else if(i == contador){
+            System.out.println("Le quedan "+(contador - i)+" intentos.");
+            System.out.println("No ha conseguido adivinar el numero :c");
+            System.out.println("El numero del programa es: "+numAleatorio);
+            System.out.print("El numero era: "+numIntroducido);
+            System.out.println("");
             }
-        
-            if (numeroIntroducido == numeroMisterioso) {
-            acertado = true;
-            System.out.println("¡Enhorabuena! ¡has acertado!");
-            }
-            oportunidades--;
-        } while (!acertado && (oportunidades > 0));
-        
-        if (!acertado) {
-            System.out.println("Lo siento, no has acertado, el numero que estaba pensando era el " + numeroIntroducido);
         }
     }
 }
