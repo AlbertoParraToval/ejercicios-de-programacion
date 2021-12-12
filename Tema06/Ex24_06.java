a5436be3353456bc67db81b7a07e606e96ac2a28
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-12 19:04:42 +0100

    Num y un digito al azar

--- Tema06/Ex24_06.java
+++ Tema06/Ex24_06.java
@@ -6,7 +6,29 @@
  */
 public class Ex24_06 {
     public static void main(String[] args) {
-
-    
+        System.out.println("El programa dado un numero introducido por teclado, elije al azar uno de sus dígitos.");
+        System.out.print("Introduce un numero entero positivo por teclado: ");
+        long numero = Integer.parseInt(System.console().readLine()) ;
+        int posicion = 0;
+        long voltear = 0;
+        int longitud = 0;
+        int digito = 0;
+        
+        //Volteo el primer numero
+        while (numero > 0){
+            voltear = (voltear * 10) + (numero %10);
+            numero /= 10;
+            longitud++; // Añadimos 1 a la longitud
+        }
+            posicion = (int)(Math.random()*(longitud +1))+1; // Posicon aleatoria en base a la longitud ya recogida antes en el voltear
+        //Recorro el numero
+        for (int i = 0; i < longitud; i++){
+            digito = (int)(voltear % 10);
+            // Cuando llegue a la posicion que muestre el numero
+            if (posicion - 1 == i){
+            System.out.println(digito);
+            }
+            voltear/=10;
+        }
     }
 }
