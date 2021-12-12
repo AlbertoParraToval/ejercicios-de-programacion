92c09bb14073a20ffe75ca022c625258a8333f32
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-12 11:48:47 +0100

    NUM ASCII

--- Tema06/Ex12_06.java
+++ Tema06/Ex12_06.java
@@ -7,9 +7,11 @@ public class Ex12_06 {
     public static void main(String[] args) {
         System.out.println("[Programa que muestra en pantalla caracteres aleatorios del codigo ASCII entre el 32 y el 126]");
         int linea = 0;        
+        //Ponemos que el bucle se repita muchisismas veces 
         for(int i = 0; i < 8000; i++) {
+          //Convertimos el char para mostrar el caracter 
           System.out.print((char)(Math.random() * (126 - 32 + 1) + 32));
-          
+          //Si al sumar 1 a linea es igual a 60
           if (linea++ == 60) {
             linea = 0;
             System.out.printf("");
