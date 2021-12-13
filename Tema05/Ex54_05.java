fee291614e7a02bcdacc1495e1640db075f7f99b
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-13 23:18:36 +0100

    Triangulo Hueco

--- Tema05/Ex54_05.java
+++ Tema05/Ex54_05.java
@@ -1,26 +1,31 @@
 /**
- *  Realiza una programa que calcule las horas transcurridas entre dos horas de
-dos días de la semana. No se tendrán en cuenta los minutos ni los segundos.
-El día de la semana se puede pedir como un número (del 1 al 7) o como una
-cadena (de lunes a domingo). Se debe comprobar que el usuario introduce
-los datos correctamente y que el segundo día es posterior al primero
+ *   Realiza un programa que pinte un triángulo hueco tal como se muestra en los
+ * ejemplos. El usuario debe introducir la altura de la figura.
 .
 
  * @author Alberto Parra Toval
  */
 public class Ex54_05 {
     public static void main(String[] args) {
-        System.out.println("El programa calcula el factorial de un número entero leído por teclado.");
-        System.out.print("Introduce un numero por teclado: ");
-        int numero = Integer.parseInt(System.console().readLine()) ;
-        int factorial = 1;
-        int multiplicarFactorial=1;
-        int contador = 1;
-        while (contador <= numero){ // Mientras contador sea menor o igual
-            factorial=contador;
-            multiplicarFactorial = multiplicarFactorial * factorial; //  multiplicarFactorial = facotira * el mismo
-            System.out.println(contador +" = "+ multiplicarFactorial); // Mostramos el contador y el resultado de multiplicarFactorial
-            contador++; //Le sumo al contador para que se acabe el bucle
+        System.out.print("Introduzca la altura de la figura: ");
+    int altura = Integer.parseInt(System.console().readLine()) ;
+    for (int i = 0; i < altura;i++){ //QPinta has que la altura sea menor que la i
+        System.out.print("*");
+    }
+    System.out.println("");
+    while (altura -2 > 0){ // Mientras altura -2 es mayor que 0
+        for (int i = altura -1; i > 0; i--){
+            if ( i == altura -1){
+            System.out.print("*");
+            }else if( i == 1 ){
+            System.out.print("*");
+            }else{
+            System.out.print(" ");
             }
+        }
+        System.out.println("");
+        altura--;
+        }
+        System.out.print("*");
     }
 }
