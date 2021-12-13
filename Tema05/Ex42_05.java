a79e3bbe1411174760e6b22fea6887bbaa9265aa
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-13 22:13:30 +0100

    Primo no Primo numeros consecutivos

--- Tema05/Ex42_05.java
+++ Tema05/Ex42_05.java
@@ -1,28 +1,33 @@
-public class Ex33_05 {
+/**
+ *  Escribe un programa que pida un número entero positivo por teclado y que mues
+ * -tre a continuación los 5 números consecutivos a partir del número introducido
+ * Al lado de cada número se debe indicar si se trata de un primo o no.
+.
+
+ * @author Alberto Parra Toval
+ */
+public class Ex42_05 {
   public static void main(String[] args) {
-    System.out.println("[Programa que dibuja una U en base a su altura]");
-    System.out.print("Introduce la altura de tu U (min 4): ");
-      int alturaIntroducida =  Integer.parseInt(System.console().readLine());
-      int anchura = alturaIntroducida - 2 ;
-      int espacios = alturaIntroducida;
-      
-      if (alturaIntroducida > 3) {
-        //Código de los dos palitos
-        for (int i = 1; i < alturaIntroducida; i++) {
-          System.out.print("*");
-          for (int j = 2; j < alturaIntroducida; j++) {
-            System.out.print("  ");
+    System.out.println("El programa calcula el factorial de un número entero leído por teclado.");
+    System.out.print("Por favor, introduzca un número entero positivo :");
+    int numero = Integer.parseInt(System.console().readLine()) ;
+    int esPrimo = 0;
+    int contador = 0;
+    boolean NoesPrimo;
+    while (contador < 5){ //numero de veces que se repite
+      NoesPrimo=false;
+      for (int i=2; i < numero; i++){
+          if (numero % i == 0){ //Si es divisible entre si mismo y es igual a 0 = NOesPrimo es cierto
+            NoesPrimo = true;
           }
-          System.out.print(" ");
-          System.out.println("*");
-        }
-        //Código de la base que depende de la altura introducida
-        for (int a = 0; a <= anchura; a++) {
-          System.out.print(" ");
-          System.out.print("*");
-          }
-      } else {
-        System.out.println("Datos erroneos");
       }
+      if(NoesPrimo){ //Si no es primo que muestre ese numero con el no primo
+        System.out.println(numero +" no es primo");
+      }else{
+        System.out.println(numero +" es primo"); // Sino pues que ponga es primo
+      }
+      numero++; //Para añadir numeros consecutivos
+      contador++; // Para salir del bucle
     }
+  }
 }
