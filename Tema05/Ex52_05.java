2de41bf95abae5b9818c632c778d9196bb7ead68
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-13 23:16:36 +0100

    Pirade Invertida

--- Tema05/Ex52_05.java
+++ Tema05/Ex52_05.java
@@ -1,60 +1,20 @@
 /**
- * Escribe un programa que calcule el factorial de un número entero leído por
- * teclado.
+ * Realiza un programa que pinte un triángulo relleno tal como se muestra en los
+ * ejemplos. El usuario debe introducir la altura de la figura.
+ * 
  * 
  * @author Alberto Parra Toval
  */
 public class Ex52_05 {
   public static void main(String[] args) {
-    System.out.println("El programa nos un numero en un sistema de palotes (Ej: 12 = | - ||)");
-    //Le pedimos al usuario el numero
-    System.out.print("Introduce un numero por teclado: ");
-    int numeroIntroducido = Integer.parseInt(System.console().readLine()) ;
-    int numero = numeroIntroducido;
-    int voltear = 0 ;
-    System.out.print("El "+ numero +" en decimal es el ");
-    //Saco la cifra del numero
-    while (numero > 0){
-      voltear = (voltear * 10) + (numero %10);
-      numero/=10;
+    System.out.print("Introduzca la altura de la figura: ");
+    int altura = Integer.parseInt(System.console().readLine()) ;
+    while (altura > 0){ //Se repite mientra la altura sea mayor a 0
+      for (int i = altura; i > 0; i--){
+        System.out.print("*");
+      }
+      System.out.println("");
+      altura--; //se le va restando para salir del bucle
     }
-
-    while (voltear > 0){
-      int cifra = voltear % 10; 
-      switch (cifra){  // Las posibles combinaciones de palos para llegar a nuestro numero introducido
-        case 1:
-          System.out.print(" | -");
-        break;
-        case 2:
-          System.out.print(" || -");
-        break;
-        case 3:
-          System.out.print(" ||| -");
-        break;
-        case 4:
-          System.out.print(" |||| -");
-        break;
-        case 5:
-          System.out.print(" ||||| -");
-        break;
-        case 6:
-          System.out.print(" |||||| -");
-        break;
-        case 7:
-          System.out.print(" ||||||| -");
-        break;
-        case 8:
-          System.out.print(" |||||||| -");
-        break;
-        case 9:
-          System.out.print(" ||||||||| -");
-        break;
-        default:
-        System.out.print("  -");
-        break;
-        } 
-      voltear /= 10;
-    }
-    System.out.print(" en el sistema palotes. "); // El final
   }
 }
