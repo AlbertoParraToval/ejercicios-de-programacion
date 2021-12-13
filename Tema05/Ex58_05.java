5fad78e991a34bd1969e3155da93150b50ec3251
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-13 23:28:23 +0100

    triangulos

--- Tema05/Ex58_05.java
+++ Tema05/Ex58_05.java
@@ -1,51 +1,32 @@
 /**
- *Escribe un programa que permita partir un número introducido por teclado en
- * dos partes. Las posiciones se cuentan de izquierda a derecha empezando por
- * el 1. Suponemos que el usuario introduce correctamente los datos, es decir,
- * el número introducido tiene dos dígitos como mínimo y la posición en la que
- * se parte el número está entre 2 y la longitud del número. No se permite en
- * este ejercicio el uso de funciones de manejo de String (por ej. para extraer
- * subcadenas dentro de una cadena).
+ *Realiza un programa que calcule la media de los dígitos que contiene un número
+ * entero introducido por teclado.
  * 
  * @author Alberto Parra Toval
  */
 
 public class Ex58_05 {
     public static void main(String[] args) {
-        System.out.println("Escribe un programa que permita partir un número introducido por teclado en dos partes");
-        System.out.print("Introduce un numero entero positivo por teclado: ");
-        int numero1 = Integer.parseInt(System.console().readLine()) ;
-        System.out.print("Introduce un numero entero positivo para partir el numero: ");
-        int posicion = Integer.parseInt(System.console().readLine()) ;
-        int voltear = 0;
-        int longitud = 0;
+        System.out.print("Introduzca un número entero : ");
+        long numero = Integer.parseInt(System.console().readLine()) ;
+        double resultado = 0;
         int digito = 0;
-        int contador = 0;
-        int numPrimeraParte = 0;
-        int numeroSegundaParte = 0;
-        if ( numero1 == 0){
-        contador=1;
-        }
+        double longitud= 0;
+        long voltear = 0;
+        /**Volteo el numero**/
+        while (numero > 0){
     
-        /**Volteo el primer numero**/
-        while (numero1 > 0){
-        voltear = (voltear * 10) + (numero1 %10);
-        numero1 /= 10;
+        voltear = (voltear * 10) + (numero %10);
+        numero /= 10;
         longitud++;
         }
-    
-        //Partir el numero
-        for (int a = 0; a < longitud; a++){
+        /**Recorro el numero**/
+        for (int i = 0; i < longitud; i++){
         digito = (int)(voltear %10);
-        //Partir la primera parte del numero
-        if (a < posicion - 1){
-        numPrimeraParte = digito + (numPrimeraParte * 10);
-        //Partir la segunda parte del numero
-        }else if (a >= posicion -1 ){
-        numeroSegundaParte = digito + (numeroSegundaParte * 10);
-        }
+        resultado = resultado + digito;
+    
         voltear/=10;
         }
-        System.out.println("Los números partidos son el "+ numPrimeraParte +" y el "+ numeroSegundaParte +".");
+        System.out.println("La media de sus dígitos es "+(resultado / longitud));
     }
 }
