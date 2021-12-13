4764065766c44d8454368323fe55e38f38fd3781
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-13 18:13:28 +0100

    Primero Pares y Luego Impares

--- Tema07/Ex10_07.java
+++ Tema07/Ex10_07.java
@@ -1,6 +1,8 @@
-/** Escribe un programa que lea 10 números por teclado y que luego los muestre
-en orden inverso, es decir, el primero que se introduce es el último en mostrarse
-y viceversa.
+/**Escribe un programa que genere 20 números enteros aleatorios entre 0 y 100 y 
+ * que los almacene en un array . El programa debe ser capaz de pasar todos los 
+ * números pares a las primeras posiciones del array(del 0 en adelante) y todos 
+ * los números impares a las celdas restantes . Utiliza arrays auxiliares si es
+ * necesario.
  *
  * @author Alberto Parra Toval
  */
