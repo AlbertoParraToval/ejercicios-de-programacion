7680ee2bcf0d75f8ee492495d3eac902a28604e2
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-13 23:02:21 +0100

    8 M

--- Tema05/Ex47_05.java
+++ Tema05/Ex47_05.java
@@ -8,30 +8,35 @@
  */
 public class Ex47_05 {
   public static void main(String[] args) {
-    System.out.print("Introduce la altura del rectangulo por teclado(min. 2): ");
+    System.out.print("Por favor, introduzca la altura (número impar mayor o igual a 5): ");
     int altura = Integer.parseInt(System.console().readLine()) ;
-    System.out.print("Introduce la anchura del rectangulo por teclado(min. 2): ");
-    int anchura = Integer.parseInt(System.console().readLine()) ;
-    if ((altura < 2) || (anchura < 2)){
-      System.out.println("Lo siento, los datos introducidos no son correctos, el valor mínimo para la anchura y la altura es 2.");
+    int anchura = 6;
+    int vuelta = 2;
+    if (altura < 5) {
+      System.out.println("La altura introducida no es correcta");
+    }else if ((altura %2) == 0){
+      System.out.println("La altura introducida no es correcta");
     }else{
-      /**Cabeza del rectangulo **/
-      for (int i = 0; i < anchura -1; i++){
-        System.out.print("*");
-      }
-      /**Cuerpo del rectangulo **/
-      for (int i = 2; i < altura; i++ ){
+      /**Cabeza del ocho **/
+        for (int i = 0; i < anchura -1; i++){
+          System.out.print("M");
+        }
+      while ( vuelta > 0){
+        /**Cuerpo del ocho **/
+        for (int i = 2; i < altura; i++ ){
+          System.out.println("");
+          System.out.print("M");
+          for (int aux = 2; aux <= (anchura - 2);aux++){
+            System.out.print(" ");
+          } 
+          System.out.print("M");
+        }
         System.out.println("");
-        System.out.print("*");
-        for (int aux = 2; aux <= (anchura - 2);aux++){
-          System.out.print(" ");
-        } 
-        System.out.print("*");
-      }
-      System.out.println("");
-      /**Base del rectangulo **/
-      for (int i = 1; i < anchura; i++){
-        System.out.print("*");
+        /**Base del ocho **/
+        for (int i = 1; i < anchura; i++){
+          System.out.print("M");
+        }
+        vuelta--;
       }
     }
   }
