2b48f0a28e581348269e5aacfc8469fe3c96cb42
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-13 23:20:57 +0100

    Un numeroa la derecha

--- Tema05/Ex55_05.java
+++ Tema05/Ex55_05.java
@@ -1,70 +1,41 @@
+/**
+ * Ejercicio: 55
+ * Realiza un programa que sea capaz de desplazar todos los dígitos de un número
+ * de izquierda a derecha una posición. El dígito de más a la derecha, pasaría a
+ * dar la vuelta y se colocaría a la izquierda.Si el número tiene un solo dígito,
+ * se queda igual.
+ * 
+ * @author Alberto Parra Toval
+ * 
+ * 
+ */
 public class Ex55_05 {
     public static void main(String[] args) {
-    System.out.print("Introduce la altura del Rombo por teclado: ");
-    int alturaIntroducida = Integer.parseInt(System.console().readLine()) ;
-    int altura =alturaIntroducida /2 +1;
-    int espaciosDelante=alturaIntroducida /2 -1;
-    int espaciosDentro= 2;
-    // Mostramos el error si la alturaIntroducida < 3
-    if ((alturaIntroducida <= 3) || ((alturaIntroducida %2)== 0)){
-      System.out.print("La altura del Rombo debe ser un numero Impar mayor a 3.");
-    }
-    System.out.println("");
-    //Parte superio del Rombo
-    if (altura == alturaIntroducida /2 +1){
-      for (int i = 1; i <= altura-1; i++){
-        System.out.print(" ");
+      System.out.print("Introduzca un número entero : ");
+      long numero = Integer.parseInt(System.console().readLine()) ;
+      long resultado = 0;
+      int digito = 0;
+      int longitud= 0;
+      long voltear = 0;
+      int cifraCambio = 0;
+      /**Volteo el numerp**/
+      while (numero > 0){
+        
+        voltear = (voltear * 10) + (numero %10);
+        numero /= 10;
+        longitud++;
       }
-      System.out.print("*");
-    }
-    System.out.println();
-    altura =1;
-    //Cuerpo superior del rombo
-    while (altura < alturaIntroducida /2 + 1){
-      
-      for (int i=1 ; i <= espaciosDelante;i++ ){
-        System.out.print(" ");
+      /**Recorro el numero**/
+      for (int i = 0; i < longitud; i++){
+        digito = (int)(voltear %10);
+        if (i == longitud -1){
+          cifraCambio=digito;
+        }else{
+          resultado = (resultado * 10) + digito;
+        }
+        voltear/=10;
       }
-      System.out.print("*");
-      for (int i = 1; i < espaciosDentro; i++ ){
-        System.out.print(" ");
-      }
-      System.out.print("*");
-      System.out.println("");
+      System.out.println("El número resultado es "+cifraCambio+""+resultado);
       
-      altura++;
-      espaciosDelante--;
-      espaciosDentro+=2;
-      
-    }
-    //Cuerpo inferior del rombo
-    altura =2;
-    espaciosDelante=1;
-    espaciosDentro= alturaIntroducida - 3;
-    
-    while (altura < alturaIntroducida /2 +1){
-      
-      for (int i=1; i <= espaciosDelante;i++ ){
-        System.out.print(" ");
-      }
-      System.out.print("*");
-      for (int i = 1; i < espaciosDentro; i++ ){
-        System.out.print(" ");
-      }
-      System.out.print("*");
-      System.out.println("");
-      
-      altura++;
-      espaciosDelante++;
-      espaciosDentro-=2;
-      
-    }
-    //Parte inferior del rombo
-    if (altura == alturaIntroducida /2 + 1){
-      for (int i = 1; i <= altura - 1; i++){
-        System.out.print(" ");
-      }
-      System.out.print("*");
     }
   }
-}
