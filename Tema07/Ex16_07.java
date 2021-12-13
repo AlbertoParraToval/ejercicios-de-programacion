116e80ba6c6ad197abecc87e47cb59eb8d1e1bad
AlbertoParraToval <alberto.parra.toval@gmail.com>
2021-12-13 19:01:24 +0100

    Multiplos de 5 o de 7

--- Tema07/Ex16_07.java
+++ Tema07/Ex16_07.java
@@ -1,27 +1,56 @@
-/**  Escribe un programa que lea 15 números por teclado y que los almacene en un
- * array. Rota los elementos de ese array, es decir, el elemento de la posición 0
- * debe pasar a la posición 1, el de la 1 a la 2, etc. El número que se encuentra
- * en la última posición debe pasar a la posición 0. Finalmente, muestra el conte
- * -nido del array.
+/**   Escribe un programa que rellene un array de 20 elementos con números enteros
+ * aleatorios comprendidos entre 0 y 400 (ambos incluidos). A continuación el
+ * programa mostrará el array y preguntará si el usuario quiere resaltar los
+ * múltiplos de 5 o los múltiplos de 7. Seguidamente se volverá a mostrar el 
+ * array escribiendo los números que se quieren resaltar entre corchetes.
  *
  * @author Alberto Parra Toval
  */
 public class Ex16_07 {
     public static void main(String[] args) {
         System.out.print("Introduce 15 numeros y el programa rota los elementos de ese array: \n");
-        int [] numero = new int[15]; // Numeros que introducimos
-        for (int i = 0; i < 15;i++){ // Que nos deje insertar hasta 15 numeros
-        numero[i] = Integer.parseInt(System.console().readLine());
-        }
-        int indice = 0; //Nuestro indice 
-        while  (indice < 15){
-        if (indice == 0 ){ // Si es igual a 0 que muestre los 14 numeros
-            System.out.print(numero[14] + " ");
-        }else{
-            System.out.print(numero[indice - 1] + " ");
-        }
-        indice++;
+        int [] numero = new int[20];
+        for (int i = 0; i < 20;i++){
+            numero[i] = (int)(Math.random()*401);
+            }
+        /**Muestra el Valor del Array Inicial**/
+        int indice = 0;
+        while  (indice < 20){
+            System.out.print(numero[indice]+" ");
+            indice++;
+            }
+
+        System.out.println("");
+        System.out.println("");
+        /**Pido la opcion y saco el numero destacado**/
+        System.out.print("¿Que numeros quiere resaltar? (1) los multiplos de 5, 2() los multiplos de 7): \n");
+        int opcion = Integer.parseInt(System.console().readLine());
+        indice = 0;
+        
+        switch (opcion){
+            case 1:
+          /**Muestra el Valor del Array Final **/
+            while  (indice < 20){
+                if ((numero[indice] % 5) == 0){
+                System.out.print("["+ numero[indice]+"] ");
+                }else{
+                System.out.print(""+numero[indice]+" ");
+                }
+                indice++;
+            }
+            break;
+            case 2:
+            /**Muestra el Valor del Array Final **/
+            while  (indice < 20){
+                if ((numero[indice] % 7) == 0){
+                    System.out.print("["+numero[indice]+"] ");
+                }else{
+                    System.out.print(""+numero[indice]+" ");
+                }
+                indice++;
+                }
+            break;
+            default:
+            }
         }
     }
-}
-    
