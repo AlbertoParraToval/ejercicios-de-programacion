b6daf590ee75b974e08cfc7b4c9b507c776a3f83
AlbertoParraToval <alberto.parra.toval@gmail.com>
2022-01-10 17:10:18 +0100

    Max y Min 6x10

---
+++ Tema07.2/Ex05_07.java
@@ -0,0 +1,68 @@
+/**  
+ *  Realiza un programa que rellene un array de 6 filas por 10 columnas
+* con números enteros positivos comprendidos entre 0 y 1000 (ambos incluidos).
+* A continuación, el programa deberá dar la posición tanto del máximo como del
+* mínimo.
+ * @author Alberto Parra Toval
+ */
+public class Ex05_07 {
+    public static void main(String[] args) throws InterruptedException  {
+        int[][] num = new int[6][10]; // array de 6 filas por 10 columnas (6x10)
+        int fila;
+        int columna;
+
+        int valorMax = Integer.MAX_VALUE;
+        int filaMaximo = 0;
+        int columnaMaximo = 0;
+
+        int valorMin = Integer.MIN_VALUE;
+        int filaMinimo = 0;
+        int columnaMinimo = 0;
+
+
+
+        System.out.print("\n      ");
+        for(columna = 0; columna < 10; columna++) {
+        System.out.print("   " + columna + "  ");
+        }
+        System.out.println();
+        
+        System.out.print("    ");
+        for(columna = 0; columna < 10; columna++) {
+            System.out.print("----------");
+        }
+        System.out.println("");
+        
+        for(fila = 0; fila < 6; fila++) {
+            System.out.print("  " + fila + " |");
+            for(columna = 0; columna < 10; columna++) {
+            num[fila][columna] = (int)(Math.random() * 1000);
+            System.out.printf("%5d ", num[fila][columna]);
+            Thread.sleep(100);
+            
+            // Calcula el mínimo y guarda sus coordenadas
+            if (num[fila][columna] < valorMin) {
+                valorMin = num[fila][columna];
+                filaMinimo = fila;
+                columnaMinimo = columna;
+                }
+            
+            // Calcula el máximo y guarda sus coordenadas
+            if (num[fila][columna] > valorMax) {
+                valorMax = num[fila][columna];
+                filaMaximo = fila;
+                columnaMaximo = columna;
+                }
+            }
+            System.out.println("|");
+            }
+            System.out.print("    ");
+        for(columna = 0; columna < 10; columna++) {
+            System.out.print("-------");
+            }
+    
+        System.out.println("\n\nEl maximo es " + valorMax + " y esta en la fila " + filaMaximo + ", columna " + columnaMaximo);
+        System.out.println("El minimo es " + valorMin  + " y esta en la fila " + filaMinimo + ", columna " + columnaMinimo);
+    }
+}
+    
