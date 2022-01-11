f14c6102edddaf06ce25bf3b712d4095a82fe7dc
AlbertoParraToval <alberto.parra.toval@gmail.com>
2022-01-11 08:44:52 +0100

    Array 3x6

--- Tema07.2/Ex01_07.java
+++ Tema07.2/Ex01_07.java
@@ -1,29 +1,32 @@
-/**  
+/**
  * Define un array de números enteros de 3 filas por 6 columnas con nombre num
-y asigna los valores según la siguiente tabla. Muestra el contenido de todos
-los elementos del array dispuestos en forma de tabla como se muestra en la
-figura.
+ * y asigna los valores según la siguiente tabla. Muestra el contenido de todos
+ * los elementos del array dispuestos en forma de tabla como se muestra en la
+ * figura.
+ * 
  * @author Alberto Parra Toval
  */
 public class Ex01_07 {
-    public static void main(String[] args) {
-        int[][] n = new int[3][6]; // array de 3 filas por 5 columnas
-            n[0][0]=0;
-            n[1][0]=75;
-            n[0][1]=30;
-            n[0][2]=2;
-            n[2][2]=-2;
-            n[2][3]=9;
-            n[1][4]=0;
-            n[0][5]=5;
-            n[2][5]=11;
-            int fila, columna;
-            for(fila = 0; fila < 3; fila++) {
-                System.out.print("Fila " + fila +":");
-                    for(columna = 0; columna <= 5; columna++) {
-                        System.out.printf(" %5d | ", n[fila][columna]);
-                        }
-            System.out.println();
-            }   
+  public static void main(String[] args) {
+    int[][] n = new int[3][6]; // array de 3 filas por 6 columnas
+    n[0][0] = 0;
+    n[1][0] = 75;
+    n[0][1] = 30;
+    n[0][2] = 2;
+    n[2][2] = -2;
+    n[2][3] = 9;
+    n[1][4] = 0;
+    n[0][5] = 5;
+    n[2][5] = 11;
+    int fila, columna; //Declaro mis variables
+    System.out.println("       Columna 1  Columna 2  Columna 3  Columna 4  Columna 5  Columna 6");
+    System.out.println("------------------------------------------------------------------------");
+    for (fila = 0; fila < 3; fila++) { //Maximo de filas
+      System.out.print("Fila " + fila + ":");
+      for (columna = 0; columna < 6; columna++) { //Maximo de columnas
+        System.out.printf(" %7d | ", n[fila][columna]);
+      }
+      System.out.println();
     }
+  }
 }
