347ba65efc7e7cac10d3eaf6bfe8eb85ff5234da
AlbertoParraToval <alberto.parra.toval@gmail.com>
2022-01-11 08:24:42 +0100

    Excel Aleatorio con Delay

--- Tema07.2/Ex04_07.java
+++ Tema07.2/Ex04_07.java
@@ -4,7 +4,7 @@
  * @author Alberto Parra Toval
  */
 public class Ex04_07 {
-    public static void main(String[] args) throws InterruptedException  {
+    public static void main(String[] args) throws InterruptedException  { //Añado esto para poder darle un pequeño delay al mostrar los datos
         int[][] num = new int[4][5]; // array de 4 filas por 5 columnas (4x5)
         int fila;
         int columna;
@@ -16,7 +16,7 @@ public class Ex04_07 {
             }
         }
     
-        // Muestra los datos y las sumas parciales de las filas
+        // Muestra los datos y la suma parcial de las filas
         int sumaFila;
         for(fila = 0; fila < 4; fila++) {
             sumaFila = 0;
@@ -29,13 +29,13 @@ public class Ex04_07 {
             System.out.printf("|%5d\n", sumaFila);
         }
         
-        //Separacion entre fila y fila
+        // La separacion entre filas
         for(columna = 0; columna < 5; columna++) {
             System.out.print("----------");
         }
         System.out.println("-----------");
     
-        //Muestra las sumas parciales de las columnas
+        //La suma parcial de las columnas
         int sumaColumna;
         int sumaTotal = 0;
         for(columna = 0; columna < 5; columna++) {
