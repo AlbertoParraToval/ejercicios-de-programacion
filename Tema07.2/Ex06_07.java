deb92b1314880d3bacd6e6cc15297fb3b0986c52
AlbertoParraToval <alberto.parra.toval@gmail.com>
2022-01-11 09:28:43 +0100

    Actualizar

--- Tema07.2/Ex06_07.java
+++ Tema07.2/Ex06_07.java
@@ -1,12 +1,10 @@
 /**
- * Realiza un programa que rellene un array de 6 filas por 10 columnas
- * con números enteros positivos comprendidos entre 0 y 1000 (ambos incluidos).
- * A continuación, el programa deberá dar la posición tanto del máximo como del
- * mínimo.
+ * Modifica el programa anterior de tal forma que no se repita ningún número en
+ * el array.
  * 
  * @author Alberto Parra Toval
  */
-public class Ex05_07 {
+public class Ex06_07 {
   public static void main(String[] args) throws InterruptedException {
     int[][] num = new int[6][10]; // array de 6 filas por 10 columnas (6x10)
     int fila;
@@ -19,7 +17,6 @@ public class Ex05_07 {
     int valorMin = Integer.MIN_VALUE;
     int filaMinimo = 0;
     int columnaMinimo = 0;
-
     System.out.print("\n      ");
     for (columna = 0; columna < 10; columna++) {
       System.out.print("   " + columna + "  ");
@@ -27,14 +24,15 @@ public class Ex05_07 {
     System.out.println();
 
     System.out.print("    ");
-    for (columna = 0; columna < 10; columna++) {
-      System.out.print("----------");
+    for (columna = 0; columna < 9; columna++) {
+      System.out.print("-------");
     }
     System.out.println("");
-
+    
     for (fila = 0; fila < 6; fila++) {
       System.out.print("  " + fila + " |");
       for (columna = 0; columna < 10; columna++) {
+        
         num[fila][columna] = (int) (Math.random() * 1000);
         System.out.printf("%5d ", num[fila][columna]);
         Thread.sleep(100);
@@ -56,13 +54,13 @@ public class Ex05_07 {
       System.out.println("|");
     }
     System.out.print("    ");
-    for (columna = 0; columna < 10; columna++) {
+    for (columna = 0; columna < 9; columna++) {
       System.out.print("-------");
     }
 
-    System.out.println(
-        "\n\nEl maximo es " + valorMax + " y esta en la fila " + filaMaximo + ", columna " + columnaMaximo);
-    System.out.println(
-        "El minimo es " + valorMin + " y esta en la fila " + filaMinimo + ", columna " + columnaMinimo);
+    System.out
+        .println("\n\nEl maximo es " + valorMax + " y esta en la fila " + filaMaximo + ", columna " + columnaMaximo);
+    System.out.println("El minimo es " + valorMin + " y esta en la fila " + filaMinimo + ", columna " + columnaMinimo);
+
   }
 }
---
+++ Tema07.2/Ex08_07.java
@@ -0,0 +1,17 @@
+/**
+ * Escribe un programa que, dada una posición en un tablero de ajedrez, nos diga
+ * a qué casillas podría saltar un alfil que se encuentra en esa posición. Como
+ * se
+ * indica en la figura, el alfil se mueve siempre en diagonal. El tablero cuenta
+ * con
+ * 64 casillas. Las columnas se indican con las letras de la “a” a la “h” y las
+ * filas
+ * se indican del 1 al 8.
+ * 
+ * @author Alberto Parra Toval
+ */
+public class Ex08_07 {
+  public static void main(String[] args) {
+    
+  }
+}
