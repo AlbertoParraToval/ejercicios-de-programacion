91f2cc6ab5f518b32819a7a5096f6193ccf0f498
AlbertoParraToval <alberto.parra.toval@gmail.com>
2022-02-08 11:01:36 +0100

    Tiempo

---
+++ Tema09/Main_06_10.java
@@ -0,0 +1,22 @@
+public class Main_06_10 {
+  public static void main(String[] args) {
+    /**
+     * Ejercicio 6 Crea la clase Tiempo con los métodos suma y resta. Los objetos de
+     * la clase Tiempo
+     * son intervalos de tiempo y se crean de la forma Tiempo t = new Tiempo(1, 20,
+     * 30)
+     * donde los parámetros que se le pasan al constructor son las horas, los
+     * minutos
+     * y los segundos respectivamente. Crea el método toString para ver los
+     * intervalos
+     * de tiempo de la forma 10h 35m 5s. Si se suman por ejemplo 30m 40s y 35m 20s
+     * el
+     * resultado debería ser 1h 6m 0s. Realiza un programa de prueba para comprobar
+     * que la clase funciona bien.
+     */
+
+    
+
+
+  }
+}
