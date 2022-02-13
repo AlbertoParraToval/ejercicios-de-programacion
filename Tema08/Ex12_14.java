e47708e1378dbcc19f06429b1a5dc7d32f351c4d
AlbertoParraToval <alberto.parra.toval@gmail.com>
2022-02-13 13:38:07 +0100

    Numero añadido delante de otro

--- Tema08/Ex12_14.java
+++ Tema08/Ex12_14.java
@@ -1,15 +1,16 @@
 import funciones.funciones_EX14;
 /**
- * PAGINA DE PRUEBA EJERCICIO 7
+ * Programa que forma un numero nuevo a partir de uno y añadiendole otro
+ * 
+ * @author Alberto Parra Toval
  */
-
 public class Ex12_14 {
   public static void main(String[] args) {
-    System.out.println("El programa nos dara la posiciÃ³n  de un ese dÃ­gito en el nÃºmero introducido.");
+    System.out.println("Programa que forma un numero nuevo a partir de uno y añadiendole otro");
     //Pedimos al usuario que nos deiga un numero y un digito
     System.out.print("Introduce un numero por teclado: ");
     Long numero = Long.parseLong(System.console().readLine());
-    System.out.print("Introduce un digito por teclado: ");
+    System.out.print("Introduce el numero que quieras añadir por delante: ");
     int digito = Integer.parseInt(System.console().readLine());
     
     System.out.println(funciones.funciones_EX14.pegaPorDelante(numero, digito));
