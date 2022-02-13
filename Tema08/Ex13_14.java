ef363cb43523c047ad58df84cde3fb347bd54f85
AlbertoParraToval <alberto.parra.toval@gmail.com>
2022-02-13 13:40:48 +0100

    Base, T.ini y T.final = Numero

--- Tema08/Ex13_14.java
+++ Tema08/Ex13_14.java
@@ -1,17 +1,18 @@
 import funciones.funciones_EX14;
 /**
- * PAGINA DE PRUEBA EJERCICIO 7
+ * Formar un numero pidiendo el numero base, la poscion inicial y la final
+ * @author Alberto Parra Toval
  */
 
 public class Ex13_14 {
   public static void main(String[] args) {
-    System.out.println("El programa nos dara la posición  de un ese dígito en el número introducido.");
+    System.out.println("Programa que forma numero con la base la parte final y la inicial");
     //Pedimos al usuario que nos deiga un numero y un digito
     System.out.print("Introduce un numero por teclado: ");
     long numero = Long.parseLong(System.console().readLine());
     System.out.println("");
 
-    System.out.print("Introduce un digito por teclado: ");
+    System.out.print("Introduce el primer trozo por teclado: ");
     int  posicionInicial = Integer.parseInt(System.console().readLine());
     System.out.println("");
 
