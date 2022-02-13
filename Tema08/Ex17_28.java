50b4b938e1a0fa9a10fab5d97b85f205a6835f23
AlbertoParraToval <alberto.parra.toval@gmail.com>
2022-02-13 13:45:49 +0100

    Binario --> Decimal

--- Tema08/Ex17_28.java
+++ Tema08/Ex17_28.java
@@ -1,5 +1,11 @@
 import funciones.funciones_EX14;
 import java.util.Scanner;
+/**
+ * 
+ * Convertidor de binario a decimal de
+ * 
+ * @author Alberto Parra Toval
+ */
 public class Ex17_28 {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in); //declaramos la variable para usar la clase Scanner
@@ -17,12 +23,7 @@ public class Ex17_28 {
     System.out.print(numeroBi + " en binario es igual a ");
 
     long numeroDe = 0; //inicializamos la variable numeroDe donde guardaremos el resultado
-    int posicion = 0; //variable que utilizaremos para indicar la psición del dígito binario (empezando por la derecha) y para usarla de exponente
-
-    //creamos un bucle while en el que numeroBi será dividido entre 10 en cada iteración hasta llegar a 0. El resto de cada división corresponde a un dígito
-    //binario, empezando por la derecha. La variable posicion indica la posición de ese número de derecha a izquierda (aumenta en 1 en cada iteración). 
-    //Una vez obtenido el dígito se multiplica por dos elevado a posicion y se le suma a lo que ya hay en numeroDe. Si digitoBi es 0 no se le sumará nada
-    //a numeroDe, por contra, si es 1, se le sumará la potencia de 2 correspondiente a la posición.
+    int posicion = 0; //variable que utilizaremos para indicar la psición del dígito binario (empezando por la derecha) y para 
     while (numeroBi > 0) {
       long digitoBi = numeroBi % 10;
       numeroDe = numeroDe + funciones.funciones_EX14.potencia(2, posicion) * digitoBi;
