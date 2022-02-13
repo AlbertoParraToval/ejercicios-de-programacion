ef3521020dc3f0ce03f82f2c145bb087bcd00f97
AlbertoParraToval <alberto.parra.toval@gmail.com>
2022-02-13 13:44:32 +0100

    Numero capicua entre 1 y 99999

--- Tema08/Ex16_28.java
+++ Tema08/Ex16_28.java
@@ -1,11 +1,16 @@
 import funciones.funciones_EX14;
+/**
+ * Programa que calcula el numero de capicuas que hay entre 1 y 99999 
+ * 
+ * @author Alberto Parra Toval
+ */
 public class Ex016_28 {
   public static void main(String[] args) {
-    System.out.println("PROGRAMA QUE MUESTRA LOS NÚMEROS CAPICÚA QUE HAY ENTRE EL 1 Y EL 99999");
+    System.out.println("PROGRAMA QUE MUESTRA LOS NUMEROS CAPICUAS QUE HAY ENTRE EL 1 Y EL 99999");
     System.out.println("-----------------------------------------------------------------------");
     System.out.println(" ");
 
-    System.out.println("Los números capicúa que hay entre el 1 y el 99999 son:\n");
+    System.out.println("Los numeros capicua que hay entre el 1 y el 99999 son:");
     //comrpobar si un nuemro es capicua
     for (int i = 1;i <= 99999; i++) {
       if (funciones.funciones_EX14.esCapicua(i)) {
