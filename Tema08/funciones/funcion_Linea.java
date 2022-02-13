f1630330e9bd287938d8386b122e24cf900c1947
AlbertoParraToval <alberto.parra.toval@gmail.com>
2022-02-13 13:10:19 +0100

    Funcion Linea

---
+++ Tema08/funciones/funcion_Linea.java
@@ -0,0 +1,18 @@
+package funciones;
+
+public class funcion_Linea {
+  public static String linea(int longitud, char extremo, char relleno) {
+    System.out.print(extremo);
+    for (int i = 0; i < longitud - 2; i++) {
+      System.out.print(relleno);
+    }
+    if (longitud > 1) {
+      System.out.print(extremo);
+    }
+    return "";
+  }
+
+  public static String espaciosPorDelante(int numespacios, char relleno) {
+    return linea(numespacios, ' ', relleno);
+  }
+}
