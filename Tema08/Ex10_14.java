4582ac64ce7b9f8e2c6ab70011fbe5f497a6ab0d
AlbertoParraToval <alberto.parra.toval@gmail.com>
2022-02-13 13:34:39 +0100

    Pos digito en un numero

--- Tema08/Ex10_14.java
+++ Tema08/Ex10_14.java
@@ -1,18 +1,18 @@
 import funciones.funciones_EX14;
 /**
- * PAGINA DE PRUEBA EJERCICIO 7
+ * Programa que nos da la posicion de un digito en el numero introducido
  */
 
 public class Ex10_14 {
   public static void main(String[] args) {
-    System.out.println("El programa nos dara la posición  de un ese dígito en el número introducido.");
+    System.out.println("El programa nos dara la posicion  de un  di�gito en el numero introducido.");
     //Pedimos al usuario que nos deiga un numero y un digito
     System.out.print("Introduce un numero por teclado: ");
     long numero = Long.parseLong(System.console().readLine());
     System.out.print("Introduce un digito por teclado: ");
     int digito = Integer.parseInt(System.console().readLine());
     
-    System.out.print("El nuevo número es " + funciones.funciones_EX14.quitaPorDelante(numero, digito));
+    System.out.print("El nuevo numero es " + funciones.funciones_EX14.quitaPorDelante(numero, digito));
   }
 }
 
