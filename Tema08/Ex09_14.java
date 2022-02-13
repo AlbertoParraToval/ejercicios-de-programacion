cd52a3b385bd4508a5ef4be6224dabc6b5006d03
AlbertoParraToval <alberto.parra.toval@gmail.com>
2022-02-13 13:33:11 +0100

    Quita digitos por delante

--- Tema08/Ex09_14.java
+++ Tema08/Ex09_14.java
@@ -1,6 +1,6 @@
 import funciones.funciones_EX14;
 /**
- * Programa que le quita a un número n dígitos por delante (por la
+ * Programa que le quita a un numero n di�gitos por delante (por la
  * izquierda)
  * 
  * @author Alberto Parra Toval
@@ -8,7 +8,7 @@ import funciones.funciones_EX14;
 
 public class Ex09_14 {
   public static void main(String[] args) {
-    System.out.println("El programa nos dara la posición  de un ese dígito en el número introducido.");
+    System.out.println("El programa nos dara la posicion  de un ese di�gito en el numero introducido.");
     //Pedimos al usuario que nos deiga un numero y un digito
     System.out.print("Introduce un numero por teclado: ");
     int numero = Integer.parseInt(System.console().readLine());
